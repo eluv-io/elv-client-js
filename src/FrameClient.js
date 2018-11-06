fd16fa3da11b35843db132e6e268d511887b627f
Kevin Talmadge <kevin@eluv.io>
2018-11-06 15:07:19 -0800

    Updated FrameClient allowed methods

--- src/FrameClient.js
+++ src/FrameClient.js
@@ -130,6 +130,7 @@ class FrameClient {
       "QParts",
       "ReplaceMetadata",
       "SetByName",
+      "SetCustomContentContract",
       "SetObjectByName",
       "UploadFileData",
       "UploadJobStatus",
