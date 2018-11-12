ee25bfb4927376c861aa35a4ec3b41ef5a43e028
Kevin Talmadge <kevin@eluv.io>
2018-11-12 12:20:09 -0800

    Updated build script to use solc 0.4.21

--- build/BuildContracts.js
+++ build/BuildContracts.js
@@ -1,24 +1,38 @@
 const fs = require("fs");
 const path = require("path");
+const solc = require("solc").setupMethods(require("./soljson-v0.4.21+commit.dfe3193c"));
 
 // Create minimal JS containing ABI and bytecode from compiled contract
 const BuildContracts = () => {
-  const contractsToBuild = [
-    "ContentLibrary",
-    "Content"
+  const contractSource = [
+    "library.sol",
+    "ownable.sol"
   ];
 
-  for (const contractFilename of contractsToBuild) {
-    const compiledContract = JSON.parse(fs.readFileSync(path.join(__dirname, "contracts", contractFilename + ".json")));
+  // Contracts to output - format: <sourcefile>:<contractname>
+  const contractKeys = [
+    "library.sol:Content",
+    "library.sol:ContentLibrary"
+  ];
+
+  let sources = {};
+  for (const contractFilename of contractSource) {
+    sources[contractFilename] = fs.readFileSync(path.join(__dirname, "..", "contracts", contractFilename)).toString();
+  }
+
+  const compilationResult = solc.compile({ sources }, 1);
+
+  for(const contractKey of contractKeys) {
+    const contractName = contractKey.split(":")[1];
 
     const contractData = {
-      abi: compiledContract.abi,
-      bytecode: compiledContract.bytecode
+      abi: JSON.parse(compilationResult.contracts[contractKey].interface),
+      bytecode: compilationResult.contracts[contractKey].bytecode
     };
 
     const contractJS = "const contract=" + JSON.stringify(contractData) + "; module.exports=contract;";
 
-    fs.writeFileSync(path.join(__dirname, "..", "src", "contracts", contractFilename + ".js"), contractJS);
+    fs.writeFileSync(path.join(__dirname, "..", "src", "contracts", contractName + ".js"), contractJS);
   }
 };
 
