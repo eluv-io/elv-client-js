79483dada7e60beaf548b71354aca99b696ced5a
Kevin Talmadge <kevin@eluv.io>
2018-11-27 12:56:49 -0800

    Added methods to set content library and object images

--- test/Test.js
+++ test/Test.js
@@ -7,34 +7,38 @@ const TestMethods = require("./TestMethods");
 const ClientConfiguration = require("../TestConfiguration.json");
 
 const Test = async () => {
-  let client = ElvClient.FromConfiguration({configuration: ClientConfiguration});
+  try {
+    let client = ElvClient.FromConfiguration({configuration: ClientConfiguration});
 
-  let wallet = client.GenerateWallet();
-  let signer = wallet.AddAccount({
-    accountName: "Alice",
-    //privateKey: "0000000000000000000000000000000000000000000000000000000000000000"
-    privateKey: "0000000000000000000000000000000000000000000000000000000000000000"
-  });
-  client.SetSigner({signer});
+    let wallet = client.GenerateWallet();
+    let signer = wallet.AddAccount({
+      accountName: "Alice",
+      //privateKey: "0000000000000000000000000000000000000000000000000000000000000000"
+      privateKey: "0000000000000000000000000000000000000000000000000000000000000000"
+    });
+    client.SetSigner({signer});
 
-  await TestMethods.TestQueries(client, signer);
+    await TestMethods.TestQueries(client, signer);
 
-  // Ensure ElvClient and FrameClient agree on allowed methods
-  const frameClient = new FrameClient({target: this});
+    // Ensure ElvClient and FrameClient agree on allowed methods
+    const frameClient = new FrameClient({target: this});
 
-  const frameClientMethods = frameClient.AllowedMethods();
-  const elvClientMethods = client.FrameAllowedMethods();
+    const frameClientMethods = frameClient.AllowedMethods();
+    const elvClientMethods = client.FrameAllowedMethods();
 
-  const differentKeys = frameClientMethods
-    .filter(x => !elvClientMethods.includes(x))
-    .concat(elvClientMethods.filter(x => !frameClientMethods.includes(x)));
+    const differentKeys = frameClientMethods
+      .filter(x => !elvClientMethods.includes(x))
+      .concat(elvClientMethods.filter(x => !frameClientMethods.includes(x)));
 
-  if(differentKeys.length > 0) {
-    console.error("MISMATCHED ALLOWED METHODS BETWEEN ELV CLIENT AND FRAME CLIENT");
-    console.error(differentKeys);
+    if (differentKeys.length > 0) {
+      console.error("MISMATCHED ALLOWED METHODS BETWEEN ELV CLIENT AND FRAME CLIENT");
+      console.error(differentKeys);
 
-    console.error("EXPECTED");
-    console.log(JSON.stringify(elvClientMethods.sort(), null, 2));
+      console.error("EXPECTED");
+      console.log(JSON.stringify(elvClientMethods.sort(), null, 2));
+    }
+  } catch(error) {
+    console.log(error);
   }
 };
 
