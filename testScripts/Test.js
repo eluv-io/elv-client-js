5d5854d5913e409976510d19903b1ac14359d3cd
Kevin Talmadge <kevin@eluv.io>
2019-04-04 17:46:22 -0700

    Cache user account library created check ; Reduce polling interval

--- testScripts/Test.js
+++ testScripts/Test.js
@@ -34,30 +34,6 @@ const Test = async () => {
 
     const libraryId = "ilib3XxtJsRr1qvM18qRGJfmcELzTX1k";
     const objectId = "iq__2XdCuga9NbAZK7NN3F4Pa3umPbAK";
-
-    const u = new UserProfileClient({client});
-
-    u.__CacheMetadata({"new": {"meta": "data", "other": "something", fuck: {"you": "up"}}});
-
-    console.log(u.cachedPrivateMetadata);
-    console.log(u.__GetCachedMetadata("new/meta"));
-    console.log(u.__GetCachedMetadata("new/meta/miss"));
-    console.log(u.__GetCachedMetadata("miss/meta/miss"));
-    console.log(u.__GetCachedMetadata("new/miss"));
-    console.log(u.__GetCachedMetadata("new/fuck/you"));
-
-    await client.userProfile.ReplacePrivateUserMetadata({metadataSubtree: "something", metadata: "value"});
-
-    console.time("call");
-    console.log("start");
-
-    let val;
-    for(let i = 0 ; i < 1000; i++) {
-      val = await client.userProfile.PrivateUserMetadata({metadataSubtree: "/something"});
-    }
-
-    console.log(val);
-    console.timeEnd("call");
   } catch(error) {
     console.error(error);
   }
