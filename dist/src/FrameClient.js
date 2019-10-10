4820cc7a56638bd54f0b6ec4217bf08db8276ea9
Kevin Talmadge <kevin@eluv.io>
2019-10-09 23:29:33 -0700

    Remove polyfill from FrameClient ; Fix event name in Commit

--- dist/src/FrameClient.js
+++ dist/src/FrameClient.js
@@ -14,8 +14,6 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-require("@babel/polyfill");
-
 require("elv-components-js/src/utils/LimitedMap");
 
 var Id = require("./Id");
