28aaa5ed595a3bd52955a42e9032ad6c43b20b92
Kevin Talmadge <kevin@eluv.io>
2019-10-25 17:04:40 -0700

    Improve master and mezzanine scripts

--- testScripts/CreateProductionMaster.js
+++ testScripts/CreateProductionMaster.js
@@ -3,9 +3,39 @@ const fs = require("fs");
 const Path = require("path");
 const mime = require("mime-types");
 
+const yargs = require("yargs");
+const argv = yargs
+  .option("library", {
+    description: "ID of the library in which to create the master"
+  })
+  .option("title", {
+    description: "Title for the master"
+  })
+  .option("metadata", {
+    description: "Metadata JSON string to include in the object metadata"
+  })
+  .option("files", {
+    type: "array",
+    description: "List of files to upload to the master object"
+  })
+  .option("s3-copy", {
+    type: "boolean",
+    description: "If specified, files will be pulled from an S3 bucket instead of the local system"
+  })
+  .option("s3-reference", {
+    type: "boolean",
+    description: "If specified, files will be referenced from an S3 bucket instead of the local system"
+  })
+  .demandOption(
+    ["library", "title", "files"],
+    "\nUsage: PRIVATE_KEY=<private-key> node CreateProductionMaster.js --library <master-library-id> --title <title> --metadata '<metadata-json>' --files <file1> (<file2>...) (--s3-copy || --s3-reference)\n"
+  )
+  .argv;
+
+
 const ClientConfiguration = require("../TestConfiguration.json");
 
-const Create = async (masterLibraryId, title, copy, filePaths) => {
+const Create = async (masterLibraryId, title, metadata, files, access, copy=false) => {
   try {
     const client = await ElvClient.FromConfigurationUrl({
       configUrl: ClientConfiguration["config-url"]
@@ -16,18 +46,9 @@ const Create = async (masterLibraryId, title, copy, filePaths) => {
     });
     await client.SetSigner({signer});
 
-    const s3 = !!process.env.AWS_KEY;
-
-    let fileInfo, access;
-    if(s3) {
-      access = {
-        region: process.env.AWS_REGION,
-        bucket: process.env.AWS_BUCKET,
-        accessKey: process.env.AWS_KEY,
-        secret: process.env.AWS_SECRET
-      };
-    } else {
-      fileInfo = filePaths.map(path => {
+    let fileInfo;
+    if(!access) {
+      fileInfo = files.map(path => {
         const data = fs.readFileSync(path);
         const mimeType = mime.lookup(path) || "video/mp4";
 
@@ -48,12 +69,13 @@ const Create = async (masterLibraryId, title, copy, filePaths) => {
         libraryId: masterLibraryId,
         name: title,
         description: "Production Master for " + title,
+        metadata,
         fileInfo,
-        filePaths,
+        filePaths: files,
         access,
         copy,
         callback: progress => {
-          if(s3) {
+          if(access) {
             console.log(progress);
           } else {
             console.log();
@@ -91,22 +113,37 @@ const Create = async (masterLibraryId, title, copy, filePaths) => {
   }
 };
 
-const masterLibraryId = process.argv[2];
-const title = process.argv[3];
-const filePaths = process.argv.slice(4);
+let {library, title, metadata, files, s3Reference, s3Copy} = argv;
 
-if(!masterLibraryId || !title || filePaths.length === 0) {
-  console.error("\nLocal File Usage:\n");
-  console.error(
-    "\tPRIVATE_KEY=<private-key> node ./testScripts/CreateProductionMaster.js <master-library-id> <title> <file-path1> (<file-path2> ...)\n"
-  );
-  console.error("\nS3 Usage:\n");
-  console.error(
-    "\tPRIVATE_KEY=<private-key> AWS_REGION=<aws-region> AWS_BUCKET=<aws-bucket> AWS_KEY=<aws-key> AWS_SECRET=<aws-secret> " +
-    "node ./testScripts/CreateProductionMaster.js <master-library-id> <title> <file-path1> (<file-path2> ...)\n"
-  );
-
+const privateKey = process.env.PRIVATE_KEY;
+if(!privateKey) {
+  console.error("PRIVATE_KEY environment variable must be specified");
   return;
 }
 
-Create(masterLibraryId, title, true, filePaths);
+let access;
+if(s3Reference || s3Copy) {
+  access = {
+    region: process.env.AWS_REGION,
+    bucket: process.env.AWS_BUCKET,
+    accessKey: process.env.AWS_KEY,
+    secret: process.env.AWS_SECRET
+  };
+
+  if(!access.region || !access.bucket || !access.accessKey || !access.secret) {
+    console.error("Missing required S3 environment variables: AWS_REGION AWS_BUCKET AWS_KEY AWS_SECRET");
+    return;
+  }
+}
+
+if(metadata) {
+  try {
+    metadata = JSON.parse(metadata);
+  } catch(error) {
+    console.error("Error parsing metadata:");
+    console.error(error);
+    return
+  }
+}
+
+Create(library, title, metadata, files, access, s3Copy && !s3Reference);
