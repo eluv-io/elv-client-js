e65a2cd36f035b678f5305dc9ef395ed33b68800
Kevin Talmadge <kevin@eluv.io>
2019-10-26 14:06:23 -0700

    Add EditContent script for modifying content

---
+++ testScripts/EditContent.js
@@ -0,0 +1,213 @@
+const { ElvClient } = require("../src/ElvClient");
+const fs = require("fs");
+const Path = require("path");
+const mime = require("mime-types");
+
+const yargs = require("yargs");
+const argv = yargs
+  .option("objectId", {
+    description: "ID of the object to modify"
+  })
+  .option("mergeMetadata", {
+    type: "array",
+    description: "Metadata to merge into the content metadata (--mergeMetadata metadata-subtree 'metadata-json')"
+  })
+  .option("replaceMetadata", {
+    type: "array",
+    description: "Metadata to replace in the content metadata (--replaceMetadata metadata-subtree 'metadata-json')"
+  })
+  .option("deleteMetadata", {
+    type: "array",
+    description: "Metadata to delete the content metadata (--deleteMetadata metadata-subtree)"
+  })
+  .option("files", {
+    type: "array",
+    description: "List of files to upload to the object"
+  })
+  .option("s3-copy", {
+    type: "boolean",
+    description: "If specified, files will be pulled from an S3 bucket instead of the local system"
+  })
+  .option("s3-reference", {
+    type: "boolean",
+    description: "If specified, files will be referenced from an S3 bucket instead of the local system"
+  })
+  .demandOption(
+    ["objectId"],
+    "\nUsage: PRIVATE_KEY=<private-key> node EditContent --objectId <object-id> --replaceMetadata <subtree> '<metadata-json>' ..."
+  )
+  .argv;
+
+const ClientConfiguration = require("../TestConfiguration.json");
+
+
+const EditContent = async ({objectId, replaceMetadata, mergeMetadata, deleteMetadata, files, access, s3Reference, s3Copy}) => {
+  const client = await ElvClient.FromConfigurationUrl({
+    configUrl: ClientConfiguration["config-url"]
+  });
+
+  let wallet = client.GenerateWallet();
+  let signer = wallet.AddAccount({
+    privateKey: process.env.PRIVATE_KEY
+  });
+
+  client.SetSigner({signer});
+
+  const libraryId = await client.ContentObjectLibraryId({objectId});
+
+  const {write_token} = await client.EditContentObject({
+    libraryId,
+    objectId
+  });
+
+  while(replaceMetadata && replaceMetadata.length > 0) {
+    const metadataSubtree = replaceMetadata.shift() || "";
+    let metadata = replaceMetadata.shift() || "";
+
+    if(!metadataSubtree || !metadata) {
+      console.error(`Invalid metadata or subtree in replaceMetadata: ${metadataSubtree} ${metadata}`);
+      return;
+    }
+
+    try {
+      metadata = JSON.parse(metadata);
+    } catch(error) {
+      console.error(`Invalid metadata in replaceMetadata: ${metadata}`);
+      console.error(error);
+    }
+
+    await client.ReplaceMetadata({
+      libraryId,
+      objectId,
+      writeToken: write_token,
+      metadataSubtree,
+      metadata
+    });
+  }
+
+  while(mergeMetadata && mergeMetadata.length > 0) {
+    const metadataSubtree = mergeMetadata.shift() || "";
+    let metadata = mergeMetadata.shift() || "";
+
+    if(!metadataSubtree || !metadata) {
+      console.error(`Invalid metadata or subtree in mergeMetadata: ${metadataSubtree} ${metadata}`);
+      return;
+    }
+
+    try {
+      metadata = JSON.parse(metadata);
+    } catch(error) {
+      console.error(`Invalid metadata in mergeMetadata: ${metadata}`);
+      console.error(error);
+    }
+
+    await client.MergeMetadata({
+      libraryId,
+      objectId,
+      writeToken: write_token,
+      metadataSubtree,
+      metadata
+    });
+  }
+
+  while(deleteMetadata && deleteMetadata.length > 0) {
+    const metadataSubtree = deleteMetadata.shift();
+
+    await client.DeleteMetadata({
+      libraryId,
+      objectId,
+      writeToken: write_token,
+      metadataSubtree
+    });
+  }
+
+  if(files) {
+    if(access) {
+      // S3 Upload
+      const {region, bucket, accessKey, secret} = access;
+
+      await client.UploadFilesFromS3({
+        libraryId,
+        objectId,
+        writeToken: write_token,
+        filePaths: files,
+        region,
+        bucket,
+        accessKey,
+        secret,
+        copy: s3Copy && !s3Reference,
+        callback: console.log
+      });
+    } else {
+      const fileInfo = files.map(path => {
+        const data = fs.readFileSync(path);
+        const mimeType = mime.lookup(path) || "video/mp4";
+
+        return {
+          path: Path.basename(path),
+          type: "file",
+          mime_type: mimeType,
+          size: data.length,
+          data
+        };
+      });
+
+      await client.UploadFiles({
+        libraryId,
+        objectId,
+        writeToken: write_token,
+        fileInfo,
+        callback: progress => {
+          console.log();
+          Object.keys(progress).forEach(filename => {
+            const percent = (100 * progress[filename].uploaded / progress[filename].total).toFixed(1);
+            console.log(`${filename}: ${percent}%`);
+          });
+        }
+      });
+    }
+
+  }
+
+  const {hash} = await client.FinalizeContentObject({
+    libraryId,
+    objectId,
+    writeToken: write_token
+  });
+
+  console.log(`\nSuccessfully created new content version: ${hash}`);
+};
+
+let {objectId, replaceMetadata, mergeMetadata, deleteMetadata, files, s3Reference, s3Copy} = argv;
+
+const privateKey = process.env.PRIVATE_KEY;
+if(!privateKey) {
+  console.error("PRIVATE_KEY environment variable must be specified");
+  return;
+}
+
+let access;
+if(s3Reference || s3Copy) {
+  access = {
+    region: process.env.AWS_REGION,
+    bucket: process.env.AWS_BUCKET,
+    accessKey: process.env.AWS_KEY,
+    secret: process.env.AWS_SECRET
+  };
+
+  if(!access.region || !access.bucket || !access.accessKey || !access.secret) {
+    console.error("Missing required S3 environment variables: AWS_REGION AWS_BUCKET AWS_KEY AWS_SECRET");
+    return;
+  }
+}
+
+EditContent({
+  objectId,
+  replaceMetadata,
+  mergeMetadata,
+  deleteMetadata,
+  files,
+  access,
+  s3Reference,
+  s3Copy
+});
