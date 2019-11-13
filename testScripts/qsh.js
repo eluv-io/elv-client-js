38c62bde6964142755dc0cd9cc0b96c601e0c1a7
elv-wayne <wayne.tran@eluv.io>
2019-11-13 16:03:03 -0500

    add Delmeta and prompt for privatekey

--- testScripts/qsh.js
+++ testScripts/qsh.js
@@ -13,10 +13,11 @@ var shellparse = require('shell-quote').parse;
 
 const commands = {
   "help" : "List commands.",
-  "access" : "Usage: access [Fabric ID].\nSets the current content to [Fabric ID]. eg iq..., ilib..., etc.",
+  "go" : "Usage: go [Fabric ID].\nSets the current content to [Fabric ID]. eg iq..., ilib..., etc.",
   "files" : "List files of the current content if applicable.",
-  "meta" : "Show content metadata.",
+  "meta" : "Usage: meta [SUBTREE].\nShow content metadata.\nSUBTREE returns only the metadata subtree (Optional).",
   "link" : "Usage: link [TARGET] [PATH]. \nCreates a link from target file to metadata path.",
+  "delmeta": "Usage delmeta [SUBTREE].\nDeletes the metadata at subtree.",
   "exit" : "Exit the shell. Same as CTRL-C.",
 }
 
@@ -59,6 +60,7 @@ const GetFiles = async ({client, objectId}) => {
     console.log("GET FILES " + objectId);
     const libraryId = await client.ContentObjectLibraryId({objectId});
     const files = await client.ListFiles({libraryId,objectId});
+    // console.log("Success.");
     return files;
   } catch(error) {
     console.error("Error getting files:");
@@ -68,11 +70,12 @@ const GetFiles = async ({client, objectId}) => {
   return null;
 }
 
-const GetMeta = async ({client, objectId}) => {
+const GetMeta = async ({client, objectId, metadataSubtree}) => {
   try {
     console.log("GET META " + objectId);
     const libraryId = await client.ContentObjectLibraryId({objectId});
-    const meta = await client.ContentObjectMetadata({libraryId, objectId});
+    const meta = await client.ContentObjectMetadata({libraryId, objectId, metadataSubtree});
+    // console.log("Success.");
     return meta;
   } catch(error) {
     console.error("Error getting files:");
@@ -80,6 +83,22 @@ const GetMeta = async ({client, objectId}) => {
   }
 
   return null;
+}
+
+const DeleteMeta = async ({client, objectId, metadataSubtree}) => {
+  try {
+    console.log("DELETE META " + objectId + " meta: " + metadataSubtree);
+    const libraryId = await client.ContentObjectLibraryId({objectId});
+    const editResult = await client.EditContentObject({libraryId, objectId});
+    const writeToken = editResult.write_token
+    console.log("writeToken: " + writeToken);
+    await client.DeleteMetadata({libraryId, objectId,writeToken,metadataSubtree});
+    await client.FinalizeContentObject({libraryId, objectId, writeToken});
+    console.log("Success.");
+  } catch(error) {
+    console.error("Error getting files:");
+    console.error(error);
+  }
 }
 
 const Link = async ({client, objectId, path, target}) =>{
@@ -122,120 +141,150 @@ const IsEmpty = (obj) =>{
   return result;
 }
 
+function AskQuestion(query,rl) {
+    return new Promise(resolve => rl.question(query, ans => {
+        resolve(ans);
+    }))
+}
+
 // MAIN Function
 const Main = async () =>{
-  const client = await ElvClient.FromConfigurationUrl({
-    configUrl: ClientConfiguration["config-url"]
-  });
 
-  let wallet = client.GenerateWallet();
-  let signer = wallet.AddAccount({
-    privateKey: process.env.PRIVATE_KEY
-  });
+  try{
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      prompt: 'QSH > '
+    });
 
-  client.SetSigner({signer});
-
-/*
-  try {
-    let result = await client.ContentObjectLibraryId({objectId:"iq__2s84rVseU6owbioNSAjN4DaJqhta"});
-    //let result = await client.ContentObjectLibraryId({objectId:"iq__2s84rvseu6owbionsajn4dajqhta"});
-    // let result = await client.ContentLibraries();
-    console.log("test result: " + result)
-  }catch(error) {
-    console.error("Error getting files:");
-    console.error(error);
-  }
-*/
-
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: 'QSH > '
-  });
-
-  let state = new State({client});
-
-  rl.on('line', async (line) => {
-    line = line.trim();
-
-    let split = shellparse(line);
-    if(IsEmpty(split)){
-      rl.prompt();
-      return;
+    let privateKey = process.env.PRIVATE_KEY;
+    if(IsEmpty(privateKey)){
+      do{
+        privateKey = await AskQuestion('Enter your Private Key: ', rl);
+      }while(IsEmpty(privateKey));
     }
 
-    // console.log("Split " + JQ(split));
+    const client = await ElvClient.FromConfigurationUrl({
+      configUrl: ClientConfiguration["config-url"]
+    });
 
-    let command = split[0].trim();
+    let wallet = client.GenerateWallet();
+    let signer = wallet.AddAccount({
+      privateKey
+    });
 
-    let args = split.slice(1);
-    command = command.toLowerCase();
+    client.SetSigner({signer});
 
-    if(command !== "access" && command !== "help" && IsEmpty(state.getCurrent())) {
-      console.log("Please 'access [Fabric ID]' first.");
-      rl.prompt();
-      return;
+  /*
+    try {
+      let result = await client.ContentObjectLibraryId({objectId:"iq__2s84rVseU6owbioNSAjN4DaJqhta"});
+      //let result = await client.ContentObjectLibraryId({objectId:"iq__2s84rvseu6owbionsajn4dajqhta"});
+      // let result = await client.ContentLibraries();
+      console.log("test result: " + result)
+    }catch(error) {
+      console.error("Error getting files:");
+      console.error(error);
     }
-    let objectId = state.getCurrent();
+  */
 
-    // console.log("objectId " + objectId + " : iq__2s84rVseU6owbioNSAjN4DaJqhta");
 
-    switch (command) {
-      case 'access':
-        if(IsEmpty(args)){
-          break;
-        }
-        await state.setCurrent(args[0]);
-        rl.setPrompt(state.getCurrent() + " >");
-        break;
-      case 'files':
-        let files = await GetFiles({objectId, client});
-        console.log(JQ(files));
-        break;
-      case 'meta':
-          const meta = await GetMeta({objectId, client});
-          console.log(JQ(meta));
-          break;
-      case 'link':
-          if(args.length != 2){
-            console.log("Link needs 2 arguments [TARGET] and [PATH]");
+    let state = new State({client});
+
+    rl.on('line', async (line) => {
+      if(IsEmpty(line)){
+          rl.prompt();
+          return;
+      }
+
+      line = line.trim();
+
+      let split = shellparse(line);
+      if(IsEmpty(split)){
+        rl.prompt();
+        return;
+      }
+
+      // console.log("Split " + JQ(split));
+
+      let command = split[0].trim();
+
+      let args = split.slice(1);
+      command = command.toLowerCase();
+
+      if(command !== "go" && command !== "help" && IsEmpty(state.getCurrent())) {
+        console.log("Please 'access [Fabric ID]' first.");
+        rl.prompt();
+        return;
+      }
+      let objectId = state.getCurrent();
+      //console.log("objectId " + objectId);
+
+      switch (command) {
+        case 'go':
+          if(IsEmpty(args)){
             break;
           }
-
-          let target = args[0];
-          let path = args[1];
-          await Link({objectId, target, path, client});
+          await state.setCurrent(args[0]);
+          rl.setPrompt(state.getCurrent() + " > ");
           break;
-      case 'help':
-          console.log('List of commands: ');
-
-          const keys = Object.keys(commands)
-          for (const key of keys) {
-            console.log("\n'" + key + "'\n" + commands[key]);
-          }
-
+        case 'files':
+          let files = await GetFiles({objectId, client});
+          console.log(JQ(files));
           break;
-      case 'exit':
-          console.log('Buh Bye!');
-          process.exit(0);
-          break;
-      case '':
-        break;
-      default:
-        console.log(`Invalid command: '${line.trim()}'`);
-        break;
-    }
+        case 'meta':
+            const meta = await GetMeta({objectId, client, metadataSubtree:args[0]});
+            console.log(JQ(meta));
+            break;
+        case 'delmeta':
+            if(args.length != 1){
+              console.log("Delmeta needs 1 argument: [METADATA SUBTREE]");
+              break;
+            }
+            let metadataSubtree = args[0];
+            await DeleteMeta({objectId, client, metadataSubtree});
+            break;
+        case 'link':
+            if(args.length != 2){
+              console.log("Link needs 2 arguments [TARGET] and [PATH]");
+              break;
+            }
 
-    console.log("\n");
+            let target = args[0];
+            let path = args[1];
+            await Link({objectId, target, path, client});
+            break;
+        case 'help':
+            console.log('List of commands: ');
+
+            const keys = Object.keys(commands)
+            for (const key of keys) {
+              console.log("\n'" + key + "'\n" + commands[key]);
+            }
+
+            break;
+        case 'exit':
+            console.log('Buh Bye!');
+            process.exit(0);
+            break;
+        case '':
+          break;
+        default:
+          console.log(`Invalid command: '${line.trim()}'`);
+          break;
+      }
+
+      console.log("\n");
+      rl.prompt();
+    }).on('close', () => {
+      console.log('Buh Bye!');
+      process.exit(0);
+    });
+
+    console.log("Welcome to the Eluvio Content Shell.\nEnter 'help' for commands.")
     rl.prompt();
-  }).on('close', () => {
-    console.log('Buh Bye!');
-    process.exit(0);
-  });
-
-  console.log("Welcome to the Eluvio Content Shell.\nEnter 'help' for commands.")
-
-  rl.prompt();
+  }catch(error) {
+    console.error(error);
+  }
 }
 
 //Execution
