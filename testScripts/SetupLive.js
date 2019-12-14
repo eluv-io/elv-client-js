f7dac90f0f5c7dd5d3d0181d875f25043ee46759
Peter Tseng <peter@eluv.io>
2019-12-13 19:10:50 -0800

    testScripts - live audio params

--- testScripts/SetupLive.js
+++ testScripts/SetupLive.js
@@ -19,27 +19,41 @@ const { ElvClient } = require("../src/ElvClient.js")
 const PRINT_DEBUG = false
 
 const hlsStream = {
+  audioTxParams: {
+    "audio_bitrate": 128000, // required
+    "audio_index": 11, // required
+    "dcodec": "aac", // required
+    "sample_rate": 48000, // Hz, required
+    // 24s @ 48000 Hz @ 1024 samples per frame (1443840 for 30.08s)
+    "seg_duration_ts": 1152000, // part size, required
+  },
   ingestType: "hls", // default: hls
   maxDurationSec: 600, // 10m, default: 2h
   originUrl: "http://yourhlsplaylist", // required for hls
   sourceTimescale: 90000, // required
-  txParams: {
-    "duration_ts": 2700000, // 30s, required for hls
+  videoTxParams: {
     "enc_height": 720, // required
     "enc_width": 1280, // required
-    "force_keyint": 50,
-    "format": "fmp4", // default: fmp4
-    "seg_duration_ts": 2700000, // 30s @ 25 fps, required
-    "video_bitrate": 8000000, // 8 Mbps
+    "force_keyint": 40, // frames, required
+    "seg_duration_ts": 2160000, // 24s @ 25 fps, part size, required
+    "video_bitrate": 8000000, // 8 Mbps, required
   },
 }
 
 const udpTsStream = {
+  audioTxParams: {
+    "audio_bitrate": 128000, // required
+    "audio_index": 0, // required
+    "dcodec": "ac3", // required
+    "ecodec": "aac", // required
+    "sample_rate": 48000, // Hz, required
+    "seg_duration_ts": 1443840, // 30s @ 48000 Hz @ 1024 samples per frame; part size, required
+  },
   ingestType: "udp",
   maxDurationSec: 600,
   sourceTimescale: 90000,
-  txParams: {
-    "duration_ts": -1, 
+  udpPort: 22001, // required for udp
+  videoTxParams: {
     "enc_height": 720,
     "enc_width": 1280,
     "force_keyint": 120,
@@ -47,12 +61,12 @@ const udpTsStream = {
     "seg_duration_ts": 2702700, // 30s @ 60000/1001 fps
     "video_bitrate": 20000000, // 20 Mbps
   },
-  udpPort: 21001 // required for udp
 }
 
 const streamParams = udpTsStream
 
 const confLocal = {
+  audioTxParams: streamParams.audioTxParams,
   clientConf: {
     configUrl: "",
     contentSpaceId: "ispc2v1uV2Lr51zyAFXeV6qi5R8628nT",
@@ -61,19 +75,17 @@ const confLocal = {
     noCache: false,
     noAuth: false
   },
-  // ingressNodeApiUrl: "http://localhost:8008",
-  // ingressNodeId: "inod2oZhwSumZZSYhaNaf77VVKCt3nu2",
-  ingressNodeApiUrl: "",
-  ingressNodeId: "",
-  libraryId: "ilib3cDtpxmqQkEgJVdihcoMphVoEBMC",
-  objectType: "hq__12hpW5caKJxSQCAjHoe73NCYzgPYZD9DNbyod7LkpCUnZBYcUXGseTMrp2KRGTToYm17xt7MNJ",
-  signerPrivateKey: process.env.PRIVATE_KEY,
-  originUrl: streamParams.originUrl,
-  txParams: streamParams.txParams,
-  sourceTimescale: streamParams.sourceTimescale,
-  maxDurationSec: streamParams.maxDurationSec,
   ingestType: streamParams.ingestType,
-  udpPort: streamParams.udpPort
+  ingressNodeApiUrl: "", // "http://localhost:8008"
+  ingressNodeId: "", // "inod2oZhwSumZZSYhaNaf77VVKCt3nu2"
+  libraryId: "ilib3cDtpxmqQkEgJVdihcoMphVoEBMC",
+  maxDurationSec: streamParams.maxDurationSec,
+  objectType: "hq__Fe22XS2BCcHBCebep6Q4jXm7UrEwxKCXeDnWKWdRr3mQvvDtHkogN7peEVyimjL7JqXfSTp4g8",
+  originUrl: streamParams.originUrl,
+  signerPrivateKey: process.env.PRIVATE_KEY,
+  sourceTimescale: streamParams.sourceTimescale,
+  udpPort: streamParams.udpPort,
+  videoTxParams: streamParams.videoTxParams,
 }
 
 /*
@@ -82,6 +94,7 @@ const confLocal = {
  * For the private key, create an account at https://core.test.contentfabric.io/#/accounts
  */
 const conf955210California = {
+  audioTxParams: streamParams.audioTxParams,
   clientConf: {
     // configUrl: "https://main.net955210.contentfabric.io",
     contentSpaceId: "ispc2zqa4gZ8N3DH1QWakR2e5UowDLF1",
@@ -90,20 +103,44 @@ const conf955210California = {
     noCache: false,
     noAuth: false
   },
+  ingestType: streamParams.ingestType,
   ingressNodeApiUrl: "https://host-35-233-145-232.test.contentfabric.io",
   ingressNodeId: "inod3jzKHgsuaBu3cqKu7t4N2fLLdkC4",
   libraryId: "ilibX37dYK6dj5F5ZQQC4eDLMNe1kZD",
-  objectType: "hq__aVeScS42SZYyRtud7ycv6UWqaxm76VGN398RamQ3w4b55Ycrn1z6DivNTJyaPhEs4m1TfR3bH",
-  signerPrivateKey: process.env.PRIVATE_KEY,
-  originUrl: streamParams.originUrl,
-  txParams: streamParams.txParams,
-  sourceTimescale: streamParams.sourceTimescale,
   maxDurationSec: streamParams.maxDurationSec,
+  objectType: "hq__aVeScS42SZYyRtud7ycv6UWqaxm76VGN398RamQ3w4b55Ycrn1z6DivNTJyaPhEs4m1TfR3bH",
+  originUrl: streamParams.originUrl,
+  signerPrivateKey: process.env.PRIVATE_KEY,
+  sourceTimescale: streamParams.sourceTimescale,
+  udpPort: streamParams.udpPort,
+  videoTxParams: streamParams.videoTxParams,
+}
+
+const conf955210Live = {
+  audioTxParams: streamParams.audioTxParams,
+  clientConf: {
+    // configUrl: "https://main.net955210.contentfabric.io",
+    contentSpaceId: "ispc2zqa4gZ8N3DH1QWakR2e5UowDLF1",
+    fabricURIs: ["https://host-38-142-50-108.test.contentfabric.io"],
+    ethereumURIs: ["https://host-38-142-50-108.test.contentfabric.io/eth/"],
+    noCache: false,
+    noAuth: false
+  },
   ingestType: streamParams.ingestType,
-  udpPort: streamParams.udpPort
+  ingressNodeApiUrl: "https://host-38-142-50-108.test.contentfabric.io",
+  ingressNodeId: "inod3o4epGxi6CKPiWjuyRUPieKCXai",
+  libraryId: "ilibX37dYK6dj5F5ZQQC4eDLMNe1kZD",
+  maxDurationSec: streamParams.maxDurationSec,
+  objectType: "hq__aVeScS42SZYyRtud7ycv6UWqaxm76VGN398RamQ3w4b55Ycrn1z6DivNTJyaPhEs4m1TfR3bH",
+  originUrl: streamParams.originUrl,
+  signerPrivateKey: process.env.PRIVATE_KEY,
+  sourceTimescale: streamParams.sourceTimescale,
+  udpPort: streamParams.udpPort,
+  videoTxParams: streamParams.videoTxParams,
 }
 
 const conf955210London = {
+  audioTxParams: streamParams.audioTxParams,
   clientConf: {
     // configUrl: "https://main.net955210.contentfabric.io",
     contentSpaceId: "ispc2zqa4gZ8N3DH1QWakR2e5UowDLF1",
@@ -112,17 +149,17 @@ const conf955210London = {
     noCache: false,
     noAuth: false
   },
+  ingestType: streamParams.ingestType,
   ingressNodeApiUrl: "https://host-35-246-28-142.test.contentfabric.io",
   ingressNodeId: "inod3wynwpbH82fsCWxbatCKxCsgyP1c",
   libraryId: "ilibX37dYK6dj5F5ZQQC4eDLMNe1kZD",
-  objectType: "hq__aVeScS42SZYyRtud7ycv6UWqaxm76VGN398RamQ3w4b55Ycrn1z6DivNTJyaPhEs4m1TfR3bH",
-  signerPrivateKey: process.env.PRIVATE_KEY,
-  originUrl: streamParams.originUrl,
-  txParams: streamParams.txParams,
-  sourceTimescale: streamParams.sourceTimescale,
   maxDurationSec: streamParams.maxDurationSec,
-  ingestType: streamParams.ingestType,
-  udpPort: streamParams.udpPort
+  objectType: "hq__aVeScS42SZYyRtud7ycv6UWqaxm76VGN398RamQ3w4b55Ycrn1z6DivNTJyaPhEs4m1TfR3bH",
+  originUrl: streamParams.originUrl,
+  signerPrivateKey: process.env.PRIVATE_KEY,
+  sourceTimescale: streamParams.sourceTimescale,
+  udpPort: streamParams.udpPort,
+  videoTxParams: streamParams.videoTxParams,
 }
 
 const conf = confLocal
@@ -199,18 +236,19 @@ const Test = async () => {
       objectId: objectId,
       writeToken: writeToken,
       metadata: {
+        "audio_tx_params": conf.audioTxParams,
         "description": "Lorem ipsum dolor sit amet",
         "edge_write_token": edgeToken,
+        "ingest_type": conf.ingestType,
         "ingress_node_api": conf.ingressNodeApiUrl,
         "ingress_node_id": conf.ingressNodeId,
+        "max_duration_sec": conf.maxDurationSec,
         "name": "Live Test - " + Date().toLocaleString(),
         "origin_url": conf.originUrl,
-        "tx_params": conf.txParams,
-        "source_timescale": conf.sourceTimescale,
-        "max_duration_sec": conf.maxDurationSec,
         "playout_type" : "live",
-        "ingest_type": conf.ingestType,
-        "udp_port": conf.udpPort
+        "source_timescale": conf.sourceTimescale,
+        "udp_port": conf.udpPort,
+        "video_tx_params": conf.videoTxParams,
       }
     })
 
@@ -229,7 +267,8 @@ const Test = async () => {
       objectId: objectId,
       versionHash: "",
       channelAuth: false,
-      noCache: true
+      noCache: true,
+      update: true,
     })
 
     const curlCmd = "curl -s -H \"$AUTH_HEADER\" "
