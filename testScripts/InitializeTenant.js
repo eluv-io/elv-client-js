d0bd83169b0e608b775c1012f588b90c42ffdd65
Kevin Talmadge <kevin@eluv.io>
2020-04-09 11:44:33 -0700

    Add error handling to InitializeTenant script

--- testScripts/InitializeTenant.js
+++ testScripts/InitializeTenant.js
@@ -33,166 +33,171 @@ const AddLibraryToGroup = async (client, libraryId, groupAddress) => {
 };
 
 const InitializeTenant = async ({configUrl, kmsId, tenantName}) => {
-  if(!process.env.PRIVATE_KEY) {
-    console.log("ERROR: 'PRIVATE_KEY' environment variable must be set");
-  }
+  try {
+    if(!process.env.PRIVATE_KEY) {
+      console.log("ERROR: 'PRIVATE_KEY' environment variable must be set");
+    }
 
-  const client = await ElvClient.FromConfigurationUrl({configUrl});
-  const wallet = client.GenerateWallet();
-  const fundedSigner = wallet.AddAccount({privateKey: process.env.PRIVATE_KEY});
+    const client = await ElvClient.FromConfigurationUrl({configUrl});
+    const wallet = client.GenerateWallet();
+    const fundedSigner = wallet.AddAccount({privateKey: process.env.PRIVATE_KEY});
 
-  const mnemonic = wallet.GenerateMnemonic();
-  const tenantAdminSigner = wallet.AddAccountFromMnemonic({mnemonic});
+    const mnemonic = wallet.GenerateMnemonic();
+    const tenantAdminSigner = wallet.AddAccountFromMnemonic({mnemonic});
 
-  // Transfer funds to new account
-  client.SetSigner({signer: fundedSigner});
-  await client.SendFunds({recipient: tenantAdminSigner.address, ether: 10});
+    // Transfer funds to new account
+    client.SetSigner({signer: fundedSigner});
+    await client.SendFunds({recipient: tenantAdminSigner.address, ether: 10});
 
-  // Switch to new tenant admin account
-  client.SetSigner({signer: tenantAdminSigner});
+    // Switch to new tenant admin account
+    client.SetSigner({signer: tenantAdminSigner});
 
-  const tenantSlug = tenantName.toLowerCase().replace(/ /g, "-");
-  await client.userProfileClient.MergeUserMetadata({
-    metadata: {
+    const tenantSlug = tenantName.toLowerCase().replace(/ /g, "-");
+    await client.userProfileClient.MergeUserMetadata({
+      metadata: {
+        public: {
+          name: `${tenantSlug}-elv-admin`
+        }
+      }
+    });
+
+    console.log("\nAdmin account:\n");
+    console.log(`\t${tenantSlug}-elv-admin`);
+    console.log(`\t${tenantAdminSigner.address}`);
+    console.log(`\t${tenantAdminSigner.signingKey.privateKey}`);
+    console.log(`\t${mnemonic}`);
+
+    // Initialize admin group
+    const adminGroupAddress = await client.CreateAccessGroup({
+      name: `${tenantName} Content Admins`
+    });
+
+    await client.AddAccessGroupManager({
+      contractAddress: adminGroupAddress,
+      memberAddress: tenantAdminSigner.address
+    });
+
+    console.log("\nContent Admins Group:\n");
+    console.log(`\t${adminGroupAddress}`);
+
+    /* Content Types - Create Title, Title Collection and Production Master and add each to the group */
+
+    const typeMetadata = {
+      bitcode_flags: "abrmaster",
+      bitcode_format: "builtin",
       public: {
-        name: `${tenantSlug}-elv-admin`
+        "eluv.displayApp": "default",
+        "eluv.manageApp": "default",
       }
-    }
-  });
+    };
 
-  console.log("\nAdmin account:\n");
-  console.log(`\t${tenantSlug}-elv-admin`);
-  console.log(`\t${tenantAdminSigner.address}`);
-  console.log(`\t${tenantAdminSigner.signingKey.privateKey}`);
-  console.log(`\t${mnemonic}`);
+    const titleTypeId = await client.CreateContentType({
+      name: `${tenantName} - Title`,
+      metadata: {...typeMetadata}
+    });
 
-  // Initialize admin group
-  const adminGroupAddress = await client.CreateAccessGroup({
-    name: `${tenantName} Content Admins`
-  });
+    await client.AddContentObjectGroupPermission({
+      objectId: titleTypeId,
+      groupAddress: adminGroupAddress,
+      permission: "manage"
+    });
 
-  await client.AddAccessGroupManager({
-    contractAddress: adminGroupAddress,
-    memberAddress: tenantAdminSigner.address
-  });
+    const titleCollectionTypeId = await client.CreateContentType({
+      name: `${tenantName} - Title Collection`,
+      metadata: {...typeMetadata}
+    });
 
-  console.log("\nContent Admins Group:\n");
-  console.log(`\t${adminGroupAddress}`);
+    await client.AddContentObjectGroupPermission({
+      objectId: titleCollectionTypeId,
+      groupAddress: adminGroupAddress,
+      permission: "manage"
+    });
 
-  /* Content Types - Create Title, Title Collection and Production Master and add each to the group */
+    const masterTypeId = await client.CreateContentType({
+      name: `${tenantName} - Title Master`,
+      metadata: {...typeMetadata}
+    });
 
-  const typeMetadata = {
-    bitcode_flags: "abrmaster",
-    bitcode_format: "builtin",
-    public: {
-      "eluv.displayApp": "default",
-      "eluv.manageApp": "default",
-    }
-  };
+    await client.AddContentObjectGroupPermission({
+      objectId: masterTypeId,
+      groupAddress: adminGroupAddress,
+      permission: "manage"
+    });
 
-  const titleTypeId = await client.CreateContentType({
-    name: `${tenantName} - Title`,
-    metadata: { ...typeMetadata }
-  });
+    console.log("\nTenant Types:\n");
+    console.log(`\t${tenantName} - Title: ${titleTypeId}`);
+    console.log(`\t${tenantName} - Title Collection: ${titleCollectionTypeId}`);
+    console.log(`\t${tenantName} - Title Master: ${masterTypeId}`);
 
-  await client.AddContentObjectGroupPermission({
-    objectId: titleTypeId,
-    groupAddress: adminGroupAddress,
-    permission: "manage"
-  });
+    /* Create libraries - Properties, Title Masters, Title Mezzanines and add each to the group */
 
-  const titleCollectionTypeId = await client.CreateContentType({
-    name: `${tenantName} - Title Collection`,
-    metadata: { ...typeMetadata }
-  });
+    const propertiesLibraryId = await client.CreateContentLibrary({
+      name: `${tenantName} - Properties`,
+      kmsId
+    });
 
-  await client.AddContentObjectGroupPermission({
-    objectId: titleCollectionTypeId,
-    groupAddress: adminGroupAddress,
-    permission: "manage"
-  });
+    await AddLibraryToGroup(client, propertiesLibraryId, adminGroupAddress);
 
-  const masterTypeId = await client.CreateContentType({
-    name: `${tenantName} - Title Master`,
-    metadata: { ...typeMetadata }
-  });
+    const mastersLibraryId = await client.CreateContentLibrary({
+      name: `${tenantName} - Title Masters`,
+      kmsId
+    });
 
-  await client.AddContentObjectGroupPermission({
-    objectId: masterTypeId,
-    groupAddress: adminGroupAddress,
-    permission: "manage"
-  });
+    await AddLibraryToGroup(client, mastersLibraryId, adminGroupAddress);
 
-  console.log("\nTenant Types:\n");
-  console.log(`\t${tenantName} - Title: ${titleTypeId}`);
-  console.log(`\t${tenantName} - Title Collection: ${titleCollectionTypeId}`);
-  console.log(`\t${tenantName} - Title Master: ${masterTypeId}`);
+    const mezzanineLibraryId = await client.CreateContentLibrary({
+      name: `${tenantName} - Title Mezzanines`,
+      kmsId
+    });
 
-  /* Create libraries - Properties, Title Masters, Title Mezzanines and add each to the group */
+    await AddLibraryToGroup(client, mezzanineLibraryId, adminGroupAddress);
 
-  const propertiesLibraryId = await client.CreateContentLibrary({
-    name: `${tenantName} - Properties`,
-    kmsId
-  });
+    console.log("\nTenant Libraries:\n");
+    console.log(`\t${tenantName} - Properties: ${propertiesLibraryId}`);
+    console.log(`\t${tenantName} - Title Masters: ${mastersLibraryId}`);
+    console.log(`\t${tenantName} - Title Mezzanines: ${mezzanineLibraryId}`);
 
-  await AddLibraryToGroup(client, propertiesLibraryId, adminGroupAddress);
+    /* Create a site object */
+    const {id, write_token} = await client.CreateContentObject({
+      libraryId: propertiesLibraryId,
+      options: {type: titleCollectionTypeId}
+    });
 
-  const mastersLibraryId = await client.CreateContentLibrary({
-    name: `${tenantName} - Title Masters`,
-    kmsId
-  });
-
-  await AddLibraryToGroup(client, mastersLibraryId, adminGroupAddress);
-
-  const mezzanineLibraryId = await client.CreateContentLibrary({
-    name: `${tenantName} - Title Mezzanines`,
-    kmsId
-  });
-
-  await AddLibraryToGroup(client, mezzanineLibraryId, adminGroupAddress);
-
-  console.log("\nTenant Libraries:\n");
-  console.log(`\t${tenantName} - Properties: ${propertiesLibraryId}`);
-  console.log(`\t${tenantName} - Title Masters: ${mastersLibraryId}`);
-  console.log(`\t${tenantName} - Title Mezzanines: ${mezzanineLibraryId}`);
-
-  /* Create a site object */
-  const { id, write_token } = await client.CreateContentObject({
-    libraryId: propertiesLibraryId,
-    options: { type: titleCollectionTypeId }
-  });
-
-  await client.ReplaceMetadata({
-    libraryId: propertiesLibraryId,
-    objectId: id,
-    writeToken: write_token,
-    metadataSubtree: "public",
-    metadata: {
-      name: `Site - ${tenantName}`,
-      asset_metadata: {
-        title: `Site - ${tenantName}`,
-        display_title: `Site - ${tenantName}`,
-        slug: `site-${tenantSlug}`,
-        title_type: "site",
-        asset_type: "primary"
+    await client.ReplaceMetadata({
+      libraryId: propertiesLibraryId,
+      objectId: id,
+      writeToken: write_token,
+      metadataSubtree: "public",
+      metadata: {
+        name: `Site - ${tenantName}`,
+        asset_metadata: {
+          title: `Site - ${tenantName}`,
+          display_title: `Site - ${tenantName}`,
+          slug: `site-${tenantSlug}`,
+          title_type: "site",
+          asset_type: "primary"
+        }
       }
-    }
-  });
+    });
 
-  await client.FinalizeContentObject({
-    libraryId: propertiesLibraryId,
-    objectId: id,
-    writeToken: write_token
-  });
+    await client.FinalizeContentObject({
+      libraryId: propertiesLibraryId,
+      objectId: id,
+      writeToken: write_token
+    });
 
-  await client.AddContentObjectGroupPermission({
-    objectId: id,
-    groupAddress: adminGroupAddress,
-    permission: "manage"
-  });
+    await client.AddContentObjectGroupPermission({
+      objectId: id,
+      groupAddress: adminGroupAddress,
+      permission: "manage"
+    });
 
-  console.log("\nSite Object: \n");
-  console.log(`\tSite - ${tenantName}: ${id}\n\n`);
+    console.log("\nSite Object: \n");
+    console.log(`\tSite - ${tenantName}: ${id}\n\n`);
+  } catch(error) {
+    console.log("Error initializing tenant:");
+    console.log(error);
+  }
 };
 
 InitializeTenant(argv);
