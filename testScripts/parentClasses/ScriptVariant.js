82d101a9dd640407da715035fe1045d0dadb7284
eponymous301 <michael.parker@eluv.io>
2020-10-21 15:07:03 -0700

    Add new oddball aspect ratio to non-4k ABR profiles Force ipTitleId to be stored as string (CreateABRMezzanine.js and CreateProductionMaster.js) Add new script ObjectDelete.js Fix class name in ObjectSetImage.js Add support for timestamps that don't have hours (OfferingAddCaptionStream.js) Add new script OfferingRemoveHls.js Add new script VariantAddStream.js Add new script VariantChangeStreamSource.js

---
+++ testScripts/parentClasses/ScriptVariant.js
@@ -0,0 +1,70 @@
+/* eslint-disable no-console */
+
+// parent class for scripts that work with variants
+
+const ScriptBase = require("./ScriptBase");
+const MetadataMixin = require("./MetadataMixin");
+
+module.exports = class ScriptVariant extends MetadataMixin(ScriptBase) {
+
+  options() {
+    return super.options()
+      .option("libraryId", {
+        alias: "library-id",
+        demandOption: true,
+        describe: "Library ID (should start with 'ilib')",
+        type: "string"
+      })
+      .option("objectId", {
+        alias: "object-id",
+        demandOption: true,
+        describe: "Object ID (should start with 'iq__')",
+        type: "string"
+      })
+      .option("variantKey", {
+        alias: "variant-key",
+        default: "default",
+        describe: "Production Master variant key",
+        type: "string"
+      });
+  }
+
+  validateVariant(metadata, variantKey) {
+    // check to make sure we have production_master
+    if(!metadata.production_master) {
+      this.throwError("Key '/production_master' not found in object metadata");
+    }
+
+    // check to make sure we have variants
+    if(!metadata.production_master.variants) {
+      this.throwError("Key '/production_master/variants' not found in object metadata");
+    }
+
+    // check for specified variant key
+    if(!metadata.production_master.variants.hasOwnProperty(variantKey)) {
+      this.throwError("Variant '" + variantKey + "' not found in production master metadata");
+    }
+  }
+
+  validateStreamSource(metadata, filePath, streamIndex) {
+    const sources = metadata.production_master.sources;
+    if(!sources.hasOwnProperty(filePath)) {
+      const sourceList = Object.keys(sources).join("\n  ");
+      this.throwError("'" + filePath + "' not found in Production Master source list. Make sure file path is correct. If you have recently added a new file to the master, try running ProductionMasterInit.js first.\n\nSources found in master:\n\n  " + sourceList);
+    }
+
+    const sourceStreams = metadata.production_master.sources[filePath].streams;
+    if(streamIndex < 0 || streamIndex >= sourceStreams.length) {
+      this.throwError("streamIndex must be between 0 and " + (sourceStreams.length - 1) + " for file '" + filePath + "'");
+    }
+
+    const sourceStream = metadata.production_master.sources[filePath].streams[streamIndex];
+    if(sourceStream.type !== "StreamAudio" && sourceStream.type !== "StreamVideo") {
+      this.throwError("streamIndex " + streamIndex + " in file '" + filePath + "' is of type '" + sourceStream.type + "', currently only StreamAudio and StreamVideo are supported");
+    }
+
+    if(sourceStream.type === "StreamAudio" && sourceStream.channels !== 2) {
+      this.throwError("streamIndex " + streamIndex + " in file '" + filePath + "' is audio but has " + sourceStream.channels + " channel(s), currently only audio streams with 2 channels are supported");
+    }
+  }
+};
