f9e46a3a6b1014113b7a1ebfdfd87bfacae3a756
eponymous301 <michael.parker@eluv.io>
2021-01-11 12:20:46 -0800

    Fix bug in unit test for concern: LRO

--- utilities/tests/unit/lib/concerns/LRO.test.js
+++ utilities/tests/unit/lib/concerns/LRO.test.js
@@ -1,19 +1,19 @@
 const {expect} = require("chai");
 
 const {loadFixture} = require("../../../helpers/fixtures");
-const {concern, contextForConcern} = require("../../../helpers/concerns");
+const {concern, concern2utility} = require("../../../helpers/concerns");
 const {removeElvEnvVars} = require("../../../helpers/params");
 
 removeElvEnvVars();
 
 const LRO = concern("LRO");
-const context = contextForConcern(LRO);
 
 describe("LRO.statusMapProcess()", () => {
 
   it(`should set run_state to ${LRO.STATE_BAD_PCT} if finished but progress < 100%`, () => {
+    const lroUtility = concern2utility(LRO, []);
     const statusMap = loadFixture("lro.status.finished.too.few.parts.fixture.json");
-    const revisedMap = context.concerns.LRO.statusMapProcess(statusMap);
+    const revisedMap = lroUtility.concerns.LRO.statusMapProcess(statusMap);
     for(const key of Object.keys(revisedMap)) {
       expect(revisedMap[key].run_state).to.equal(LRO.STATE_BAD_PCT);
     }
