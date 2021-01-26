707c055b4803cb4744f528bb4029c2c487692cb4
eponymous301 <michael.parker@eluv.io>
2021-01-26 15:22:33 -0800

    /utilities  * Add --filter option to LibraryListObjects.js to filter objects by (public) metadata  * minor output change to integration test test.offering.copy.and.text.watermark.sh

--- utilities/LibraryListObjects.js
+++ utilities/LibraryListObjects.js
@@ -1,23 +1,37 @@
 // List all object IDs in a library
 
-const {ModOpt} = require("./lib/options");
+const {ModOpt, NewOpt} = require("./lib/options");
 const Utility = require("./lib/Utility");
 
+const JSON = require("./lib/concerns/JSON");
 const Library = require("./lib/concerns/Library");
 
 class LibraryListObjects extends Utility {
   blueprint() {
     return {
-      concerns: [Library],
+      concerns: [JSON, Library],
       options: [
-        ModOpt("libraryId", {demand: true})
+        ModOpt("libraryId", {demand: true}),
+        NewOpt("filter", {
+          descTemplate: "JSON expression (or path to JSON file if starting with '@') to filter objects by metadata",
+          type: "string"
+        })
       ]
     };
   }
 
   async body() {
     const logger = this.logger;
-    const objectList = await this.concerns.Library.objectList({filterOptions:{select:["/public/name"]}});
+    const filter = this.args.filter && this.concerns.JSON.parseStringOrFile(this.args.filter);
+
+    const objectList = await this.concerns.Library.objectList(
+      {
+        filterOptions: {
+          select: ["/public/name", "/offerings"],
+          filter
+        }
+      }
+    );
     const formattedObjList = objectList.map(x => ({
       object_id: x.objectId,
       latest_hash: x.latestHash,
