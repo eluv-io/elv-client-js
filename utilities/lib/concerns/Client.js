5fb9ce3b3f7ea0a0535a9437d1f555a586ef75b1
eponymous301 <michael.parker@eluv.io>
2021-01-27 10:54:10 -0800

    /utilities   Add note to --configUrl helptext about using FABRIC_CONFIG_URL

--- utilities/lib/concerns/Client.js
+++ utilities/lib/concerns/Client.js
@@ -11,7 +11,7 @@ const blueprint = {
   concerns: [Logger],
   options: [
     NewOpt("configUrl", {
-      descTemplate: "URL to query for Fabric configuration, enclosed in quotes - e.g. for Eluvio demo network: --configUrl \"https://demov3.net955210.contentfabric.io/config\"",
+      descTemplate: "URL to query for Fabric configuration, enclosed in quotes - e.g. for Eluvio demo network: --configUrl \"https://demov3.net955210.contentfabric.io/config\" (you can choose to set env var FABRIC_CONFIG_URL instead)",
       group: "API",
       type: "string"
     }),
