95438677ac8ebf4bf869223b8deb30ab2ef32450
eponymous301 <michael.parker@eluv.io>
2021-02-21 13:35:33 -0800

    /utilities  * New script ObjectSetPermission.js

---
+++ utilities/ObjectSetPermission.js
@@ -0,0 +1,61 @@
+// Give a group permissions to an object
+const {ModOpt, NewOpt} = require("./lib/options");
+
+const Utility = require("./lib/Utility");
+
+const Client = require("./lib/concerns/Client");
+const ArgObjectId = require("./lib/concerns/ArgObjectId");
+const Logger = require("./lib/concerns/Logger");
+
+const permissionTypes = ["owner", "editable", "viewable", "listable", "public"];
+
+class ObjectAddGroupPerms extends Utility {
+  blueprint() {
+    return {
+      concerns: [Logger, ArgObjectId, Client],
+      options: [
+        ModOpt("objectId", {demand: true, X: " to set permission on"}),
+        NewOpt("permission", {
+          choices: permissionTypes,
+          demand: true,
+          descTemplate: "Permission level to set",
+          type: "string"
+        })
+      ]
+    };
+  }
+
+  async body() {
+    const logger = this.concerns.Logger;
+    const client = await this.concerns.Client.get();
+
+    const {objectId, permission} = await this.concerns.ArgObjectId.argsProc();
+
+    const prevHash = await client.LatestVersionHash({objectId: objectId});
+
+    await client.SetPermission({
+      objectId,
+      permission
+    });
+
+    const newHash = await client.LatestVersionHash({objectId: objectId});
+
+    if(prevHash === newHash) {
+      logger.log("Version hash unchanged: " + newHash );
+    } else {
+      logger.log("Previous version hash: " + prevHash );
+      logger.log("New version hash: " + newHash );
+    }
+    logger.data("version_hash", newHash);
+  }
+
+  header() {
+    return `Set permission on ${this.args.objectId} to ${this.args.permission}`;
+  }
+}
+
+if(require.main === module) {
+  Utility.cmdLineInvoke(ObjectAddGroupPerms);
+} else {
+  module.exports = ObjectAddGroupPerms;
+}
