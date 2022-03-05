998475dddc215be9e6a6053441c819f6cd0a01af
kevin.talmadge@eluv.io <kevin@eluv.io>
2022-03-04 16:27:46 -0800

    Switch back to using signed noAuth tokens by default ; Add unsignedPublicAuth flag to remote signer ; Cache signDigest method in remote signer

--- dist/src/RemoteSigner.js
+++ dist/src/RemoteSigner.js
@@ -34,12 +34,15 @@ function (_Ethers$Signer) {
         tenantId = _ref.tenantId,
         provider = _ref.provider,
         _ref$extraData = _ref.extraData,
-        extraData = _ref$extraData === void 0 ? {} : _ref$extraData;
+        extraData = _ref$extraData === void 0 ? {} : _ref$extraData,
+        _ref$unsignedPublicAu = _ref.unsignedPublicAuth,
+        unsignedPublicAuth = _ref$unsignedPublicAu === void 0 ? false : _ref$unsignedPublicAu;
 
     _classCallCheck(this, RemoteSigner);
 
     _this = _possibleConstructorReturn(this, _getPrototypeOf(RemoteSigner).call(this));
     _this.remoteSigner = true;
+    _this.unsignedPublicAuth = unsignedPublicAuth;
     _this.HttpClient = new HttpClient({
       uris: rpcUris
     });
@@ -48,6 +51,7 @@ function (_Ethers$Signer) {
     _this.authToken = authToken;
     _this.extraLoginData = extraData || {};
     _this.provider = provider;
+    _this.signatureCache = {};
     return _this;
   }
 
@@ -140,32 +144,55 @@ function (_Ethers$Signer) {
   }, {
     key: "signDigest",
     value: function signDigest(digest) {
-      var signature;
-      return _regeneratorRuntime.async(function signDigest$(_context2) {
+      var _this2 = this;
+
+      return _regeneratorRuntime.async(function signDigest$(_context3) {
         while (1) {
-          switch (_context2.prev = _context2.next) {
+          switch (_context3.prev = _context3.next) {
             case 0:
-              _context2.next = 2;
-              return _regeneratorRuntime.awrap(Utils.ResponseToJson(this.HttpClient.Request({
-                method: "POST",
-                path: UrlJoin("as", "wlt", "sign", "eth", this.id),
-                headers: {
-                  Authorization: "Bearer ".concat(this.authToken)
-                },
-                body: {
-                  hash: digest
-                }
-              })));
+              if (!this.signatureCache[digest]) {
+                this.signatureCache[digest] = new Promise(function _callee(resolve) {
+                  var signature;
+                  return _regeneratorRuntime.async(function _callee$(_context2) {
+                    while (1) {
+                      switch (_context2.prev = _context2.next) {
+                        case 0:
+                          _context2.next = 2;
+                          return _regeneratorRuntime.awrap(Utils.ResponseToJson(_this2.HttpClient.Request({
+                            method: "POST",
+                            path: UrlJoin("as", "wlt", "sign", "eth", _this2.id),
+                            headers: {
+                              Authorization: "Bearer ".concat(_this2.authToken)
+                            },
+                            body: {
+                              hash: digest
+                            }
+                          })));
 
-            case 2:
-              signature = _context2.sent;
-              signature.v = parseInt(signature.v, 16);
-              signature.recoveryParam = signature.v - 27;
-              return _context2.abrupt("return", signature);
+                        case 2:
+                          signature = _context2.sent;
+                          signature.v = parseInt(signature.v, 16);
+                          signature.recoveryParam = signature.v - 27;
+                          resolve(signature);
 
-            case 6:
+                        case 6:
+                        case "end":
+                          return _context2.stop();
+                      }
+                    }
+                  });
+                });
+              }
+
+              _context3.next = 3;
+              return _regeneratorRuntime.awrap(this.signatureCache[digest]);
+
+            case 3:
+              return _context3.abrupt("return", _context3.sent);
+
+            case 4:
             case "end":
-              return _context2.stop();
+              return _context3.stop();
           }
         }
       }, null, this);
@@ -173,25 +200,25 @@ function (_Ethers$Signer) {
   }, {
     key: "signMessage",
     value: function signMessage(message) {
-      return _regeneratorRuntime.async(function signMessage$(_context3) {
+      return _regeneratorRuntime.async(function signMessage$(_context4) {
         while (1) {
-          switch (_context3.prev = _context3.next) {
+          switch (_context4.prev = _context4.next) {
             case 0:
-              _context3.t0 = Promise;
-              _context3.t1 = Ethers.utils;
-              _context3.t2 = "0x";
-              _context3.next = 5;
+              _context4.t0 = Promise;
+              _context4.t1 = Ethers.utils;
+              _context4.t2 = "0x";
+              _context4.next = 5;
               return _regeneratorRuntime.awrap(this.signDigest(Ethers.utils.hashMessage(message)));
 
             case 5:
-              _context3.t3 = _context3.sent;
-              _context3.t4 = _context3.t2.concat.call(_context3.t2, _context3.t3);
-              _context3.t5 = _context3.t1.joinSignature.call(_context3.t1, _context3.t4);
-              return _context3.abrupt("return", _context3.t0.resolve.call(_context3.t0, _context3.t5));
+              _context4.t3 = _context4.sent;
+              _context4.t4 = _context4.t2.concat.call(_context4.t2, _context4.t3);
+              _context4.t5 = _context4.t1.joinSignature.call(_context4.t1, _context4.t4);
+              return _context4.abrupt("return", _context4.t0.resolve.call(_context4.t0, _context4.t5));
 
             case 9:
             case "end":
-              return _context3.stop();
+              return _context4.stop();
           }
         }
       }, null, this);
@@ -200,25 +227,25 @@ function (_Ethers$Signer) {
     key: "sign",
     value: function sign(transaction) {
       var signature;
-      return _regeneratorRuntime.async(function sign$(_context4) {
+      return _regeneratorRuntime.async(function sign$(_context5) {
         while (1) {
-          switch (_context4.prev = _context4.next) {
+          switch (_context5.prev = _context5.next) {
             case 0:
-              _context4.next = 2;
+              _context5.next = 2;
               return _regeneratorRuntime.awrap(Ethers.utils.resolveProperties(transaction));
 
             case 2:
-              transaction = _context4.sent;
-              _context4.next = 5;
+              transaction = _context5.sent;
+              _context5.next = 5;
               return _regeneratorRuntime.awrap(this.signDigest(Ethers.utils.keccak256(Ethers.utils.serializeTransaction(transaction))));
 
             case 5:
-              signature = _context4.sent;
-              return _context4.abrupt("return", Ethers.utils.serializeTransaction(transaction, signature));
+              signature = _context5.sent;
+              return _context5.abrupt("return", Ethers.utils.serializeTransaction(transaction, signature));
 
             case 7:
             case "end":
-              return _context4.stop();
+              return _context5.stop();
           }
         }
       }, null, this);
@@ -226,34 +253,34 @@ function (_Ethers$Signer) {
   }, {
     key: "sendTransaction",
     value: function sendTransaction(transaction) {
-      var _this2 = this;
+      var _this3 = this;
 
-      return _regeneratorRuntime.async(function sendTransaction$(_context5) {
+      return _regeneratorRuntime.async(function sendTransaction$(_context6) {
         while (1) {
-          switch (_context5.prev = _context5.next) {
+          switch (_context6.prev = _context6.next) {
             case 0:
               if (!(transaction.nonce == null)) {
-                _context5.next = 5;
+                _context6.next = 5;
                 break;
               }
 
               transaction = Ethers.utils.shallowCopy(transaction);
-              _context5.next = 4;
+              _context6.next = 4;
               return _regeneratorRuntime.awrap(this.provider.getTransactionCount(this.address, "pending"));
 
             case 4:
-              transaction.nonce = _context5.sent;
+              transaction.nonce = _context6.sent;
 
             case 5:
-              return _context5.abrupt("return", Ethers.utils.populateTransaction(transaction, this.provider, this.address).then(function (tx) {
-                return _this2.sign(tx).then(function (signedTransaction) {
-                  return _this2.provider.sendTransaction(signedTransaction);
+              return _context6.abrupt("return", Ethers.utils.populateTransaction(transaction, this.provider, this.address).then(function (tx) {
+                return _this3.sign(tx).then(function (signedTransaction) {
+                  return _this3.provider.sendTransaction(signedTransaction);
                 });
               }));
 
             case 6:
             case "end":
-              return _context5.stop();
+              return _context6.stop();
           }
         }
       }, null, this);
