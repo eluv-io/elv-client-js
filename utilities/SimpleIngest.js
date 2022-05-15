929ddb4d217028581fe5c87d6eb89110e0e9688c
elvwayne <wayne@eluv.io>
2022-05-14 17:34:38 -0700

    Add generating image and embed_url from new ingest object and writing to json file if it exists for elv-live tool nft_build

--- utilities/SimpleIngest.js
+++ utilities/SimpleIngest.js
@@ -1,7 +1,7 @@
 // Create new production master from specified file(s)
 const R = require("ramda");
 
-const {ModOpt, NewOpt} = require("./lib/options");
+const { ModOpt, NewOpt } = require("./lib/options");
 const Utility = require("./lib/Utility");
 
 const ABR = require("@eluvio/elv-abr-profile");
@@ -11,26 +11,36 @@ const Finalize = require("./lib/concerns/Finalize");
 const LocalFile = require("./lib/concerns/LocalFile");
 const LRO = require("./lib/concerns/LRO");
 const ArgLibraryId = require("./lib/concerns/ArgLibraryId");
-const {seconds} = require("./lib/helpers");
+const { readFile } = require("./lib/helpers");
+const { seconds } = require("./lib/helpers");
+var UrlJoin = require("url-join");
+const path = require("path");
+const fs = require("fs");
 
 class SimpleIngest extends Utility {
   blueprint() {
     return {
       concerns: [Client, Finalize, LocalFile, ArgLibraryId, LRO],
       options: [
-        ModOpt("libraryId", {demand: true, forX: "new media object"}),
+        ModOpt("libraryId", { demand: true, forX: "new media object" }),
         NewOpt("title", {
           demand: true,
           descTemplate: "Title for new media object",
-          type: "string"
+          type: "string",
+        }),
+        NewOpt("description", {
+          demand: false,
+          descTemplate: "Description for the new media object",
+          default: "",
+          type: "string",
         }),
         NewOpt("drm", {
           default: false,
           descTemplate: "Use DRM for playback",
-          type: "boolean"
+          type: "boolean",
         }),
-        ModOpt("files", {forX: "for new media object"})
-      ]
+        ModOpt("files", { forX: "for new media object" }),
+      ],
     };
   }
 
@@ -48,12 +58,17 @@ class SimpleIngest extends Utility {
     const libInfo = await this.concerns.ArgLibraryId.libInfo();
 
     const type = R.path(["metadata", "abr", "mez_content_type"], libInfo);
-    if(R.isNil(type)) throw Error("Library does not specify content type for simple ingests");
+    if (R.isNil(type))
+      throw Error("Library does not specify content type for simple ingests");
 
-    const libABRProfile = R.path(["metadata", "abr", "default_profile"], libInfo);
-    if(R.isNil(libABRProfile)) throw Error("Library does not specify ABR profile for simple ingests");
+    const libABRProfile = R.path(
+      ["metadata", "abr", "default_profile"],
+      libInfo
+    );
+    if (R.isNil(libABRProfile))
+      throw Error("Library does not specify ABR profile for simple ingests");
 
-    const {drm, libraryId, title} = this.args;
+    const { drm, libraryId, title, description, configUrl } = this.args;
     const encrypt = true;
 
     logger.log("Uploading files...");
@@ -62,14 +77,14 @@ class SimpleIngest extends Utility {
       libraryId,
       type,
       name: title,
-      description: `Media object created via simple ingest: ${title}`,
+      description,
       fileInfo,
       encrypt,
       copy: true,
-      callback: this.concerns.LocalFile.callback
+      callback: this.concerns.LocalFile.callback,
     });
 
-    const {id, hash} = createMasterResponse;
+    const { id, hash } = createMasterResponse;
     // Log object id immediately, in case of error later in script
     // Don't log hash yet, it will change if --streams was provided (or any other revision to object is needed)
     logger.data("object_id", id);
@@ -89,43 +104,64 @@ class SimpleIngest extends Utility {
 
     logger.data("version_hash", hash);
 
-    if(!R.isNil(createMasterResponse.errors) && !R.isEmpty(createMasterResponse.errors)) throw Error(`Error(s) encountered while inspecting uploaded files: ${createMasterResponse.errors.join("\n")}`);
+    if (
+      !R.isNil(createMasterResponse.errors) &&
+      !R.isEmpty(createMasterResponse.errors)
+    )
+      throw Error(
+        `Error(s) encountered while inspecting uploaded files: ${createMasterResponse.errors.join(
+          "\n"
+        )}`
+      );
 
     await seconds(2);
 
     await this.concerns.Finalize.waitForPublish({
       latestHash: hash,
       libraryId,
-      objectId: id
+      objectId: id,
     });
 
-
     // get production master metadata
-    const masterMetadata = (await client.ContentObjectMetadata({
+    const masterMetadata = await client.ContentObjectMetadata({
       libraryId,
       objectId: id,
       versionHash: hash,
-      metadataSubtree: "/production_master"
-    }));
+      metadataSubtree: "/production_master",
+    });
 
     const sources = R.prop("sources", masterMetadata);
     const variant = R.path(["variants", "default"], masterMetadata);
 
     // add info on source files and variant to data if --json selected
-    if(this.args.json) {
+    if (this.args.json) {
       logger.data("media_files", sources);
       logger.data("variant_default", variant);
     }
 
     // generate ABR profile
-    const genProfileRetVal = ABR.ABRProfileForVariant(sources, variant, libABRProfile);
-    if(!genProfileRetVal.ok) throw Error(`Error(s) encountered while generating ABR profile: ${genProfileRetVal.errors.join("\n")}`);
+    const genProfileRetVal = ABR.ABRProfileForVariant(
+      sources,
+      variant,
+      libABRProfile
+    );
+    if (!genProfileRetVal.ok)
+      throw Error(
+        `Error(s) encountered while generating ABR profile: ${genProfileRetVal.errors.join(
+          "\n"
+        )}`
+      );
 
     // filter DRM/clear as needed
-    const filterProfileRetVal = drm ?
-      ABR.ProfileExcludeClear(genProfileRetVal.result) :
-      ABR.ProfileExcludeDRM(genProfileRetVal.result);
-    if(!filterProfileRetVal.ok) throw Error(`Error(s) encountered while setting playout formats: ${filterProfileRetVal.errors.join("\n")}`);
+    const filterProfileRetVal = drm
+      ? ABR.ProfileExcludeClear(genProfileRetVal.result)
+      : ABR.ProfileExcludeDRM(genProfileRetVal.result);
+    if (!filterProfileRetVal.ok)
+      throw Error(
+        `Error(s) encountered while setting playout formats: ${filterProfileRetVal.errors.join(
+          "\n"
+        )}`
+      );
 
     // set up mezzanine offering
     logger.log("Setting up media file conversion...");
@@ -137,33 +173,45 @@ class SimpleIngest extends Utility {
       masterVersionHash: hash,
       variant: "default",
       offeringKey: "default",
-      abrProfile: filterProfileRetVal.result
+      abrProfile: filterProfileRetVal.result,
     });
 
     logger.errorsAndWarnings(createMezResponse);
     const createMezErrors = createMezResponse.errors;
-    if(!R.isNil(createMezErrors) && !R.isEmpty(createMezErrors)) throw Error(`Error(s) encountered while setting up media file conversion: ${createMezErrors.join("\n")}`);
+    if (!R.isNil(createMezErrors) && !R.isEmpty(createMezErrors))
+      throw Error(
+        `Error(s) encountered while setting up media file conversion: ${createMezErrors.join(
+          "\n"
+        )}`
+      );
 
     await this.concerns.Finalize.waitForPublish({
       latestHash: createMezResponse.hash,
       libraryId,
-      objectId: id
+      objectId: id,
     });
-
 
     logger.log("Starting conversion to streaming format...");
 
     const startJobsResponse = await client.StartABRMezzanineJobs({
       libraryId,
       objectId: id,
-      offeringKey: "default"
+      offeringKey: "default",
     });
 
     logger.errorsAndWarnings(startJobsResponse);
     const startJobsErrors = createMezResponse.errors;
-    if(!R.isNil(startJobsErrors) && !R.isEmpty(startJobsErrors)) throw Error(`Error(s) encountered while starting file conversion: ${startJobsErrors.join("\n")}`);
+    if (!R.isNil(startJobsErrors) && !R.isEmpty(startJobsErrors))
+      throw Error(
+        `Error(s) encountered while starting file conversion: ${startJobsErrors.join(
+          "\n"
+        )}`
+      );
 
-    const lroWriteToken = R.path(["lro_draft", "write_token"], startJobsResponse);
+    const lroWriteToken = R.path(
+      ["lro_draft", "write_token"],
+      startJobsResponse
+    );
     const lroNode = R.path(["lro_draft", "node"], startJobsResponse);
 
     logger.data("library_id", libraryId);
@@ -188,7 +236,7 @@ class SimpleIngest extends Utility {
     await this.concerns.Finalize.waitForPublish({
       latestHash: startJobsResponse.hash,
       libraryId,
-      objectId: id
+      objectId: id,
     });
 
     logger.log("Progress:");
@@ -196,27 +244,32 @@ class SimpleIngest extends Utility {
     const lro = this.concerns.LRO;
     let done = false;
     let lastStatus;
-    while(!done) {
-      const statusMap = await lro.status({libraryId, objectId: id}); // TODO: check how offering key is used, if at all
+    while (!done) {
+      const statusMap = await lro.status({ libraryId, objectId: id }); // TODO: check how offering key is used, if at all
       const statusSummary = lro.statusSummary(statusMap);
       lastStatus = statusSummary.run_state;
-      if(lastStatus !== LRO.STATE_RUNNING) done = true;
+      if (lastStatus !== LRO.STATE_RUNNING) done = true;
       logger.log(`run_state: ${lastStatus}`);
       const eta = statusSummary.estimated_time_left_h_m_s;
-      if(eta) logger.log(`estimated time left: ${eta}`);
+      if (eta) logger.log(`estimated time left: ${eta}`);
       await seconds(15);
     }
 
     const finalizeAbrResponse = await client.FinalizeABRMezzanine({
       libraryId,
       objectId: id,
-      offeringKey: "default"
+      offeringKey: "default",
     });
     const latestHash = finalizeAbrResponse.hash;
 
     logger.errorsAndWarnings(finalizeAbrResponse);
     const finalizeErrors = finalizeAbrResponse.errors;
-    if(!R.isNil(finalizeErrors) && !R.isEmpty(finalizeErrors)) throw Error(`Error(s) encountered while finalizing object: ${finalizeErrors.join("\n")}`);
+    if (!R.isNil(finalizeErrors) && !R.isEmpty(finalizeErrors))
+      throw Error(
+        `Error(s) encountered while finalizing object: ${finalizeErrors.join(
+          "\n"
+        )}`
+      );
 
     logger.logList(
       "",
@@ -225,12 +278,54 @@ class SimpleIngest extends Utility {
       `  Version Hash: ${latestHash}`,
       ""
     );
+
     logger.data("version_hash", latestHash);
     await this.concerns.Finalize.waitForPublish({
       latestHash,
       libraryId,
-      objectId: id
+      objectId: id,
     });
+
+    let imageUrl = await client.FabricUrl({
+      versionHash: latestHash,
+      noAuth: true,
+    });
+    imageUrl = UrlJoin(
+      lroNode,
+      "q",
+      latestHash,
+      "meta/public/asset_metadata/images/img/default"
+    );
+
+    let embedUrl = CreateEmbedUrl({
+      versionHash: latestHash,
+      network: GetNet({ configUrl }),
+    });
+
+    logger.logList(`embed_url: ${embedUrl}`, `image: ${imageUrl}`);
+
+    let filepath = path.parse(fileInfo[0].fullPath);
+    let jsonFile = path.join(filepath.dir, filepath.name) + ".json";
+    logger.log("Looking for Json file: ", jsonFile);
+
+    let jsonFileContents = null;
+
+    if (fs.existsSync(jsonFile)) {
+      try {
+        jsonFileContents = JSON.parse(fs.readFileSync(jsonFile));
+        logger.log("JSON File found: ", jsonFileContents);
+        jsonFileContents.name = title;
+        jsonFileContents.display_name = title;
+        jsonFileContents.description = description;
+
+        let jsonString = JSON.stringify(jsonFileContents, "", 2);
+        fs.writeFileSync(jsonFile, jsonString);
+        logger.log("Json file updated: ", jsonString);
+        //file written successfully
+      } catch (err) {
+        logger.warn("Could not read or write to json file", err);
+      }
+    }
   }
 
   header() {
@@ -238,7 +333,21 @@ class SimpleIngest extends Utility {
   }
 }
 
-if(require.main === module) {
+const CreateEmbedUrl = ({ versionHash, network }) => {
+  return `https://embed.v3.contentfabric.io/?p=&net=${network}&vid=${versionHash}&m=&ap=&lp=`;
+};
+
+const GetNet = ({ configUrl }) => {
+  if (configUrl.includes("demo")) {
+    return "demo";
+  } else if (configUrl.includes("test")) {
+    return "test";
+  } else {
+    return "main";
+  }
+};
+
+if (require.main === module) {
   Utility.cmdLineInvoke(SimpleIngest);
 } else {
   module.exports = SimpleIngest;
--- utilities/lib/concerns/LocalFile.js
+++ utilities/lib/concerns/LocalFile.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 const path = require("path");
 const mime = require("mime-types");
 
-const {absPath} = require("../helpers");
-const {StdOpt} = require("../options");
+const { absPath } = require("../helpers");
+const { StdOpt } = require("../options");
 
 const Client = require("./Client");
 const Logger = require("./Logger");
@@ -12,14 +12,17 @@ const Logger = require("./Logger");
 const blueprint = {
   name: "LocalFile",
   concerns: [Logger, Client],
-  options: [
-    StdOpt("files", {demand: true}),
-  ]
+  options: [StdOpt("files", { demand: true })],
 };
 
-const New = context => {
-
-  const add = async ({libraryId, objectId, writeToken, fileInfo, encrypt}) => {
+const New = (context) => {
+  const add = async ({
+    libraryId,
+    objectId,
+    writeToken,
+    fileInfo,
+    encrypt,
+  }) => {
     const client = await context.concerns.Client.get();
     await client.UploadFiles({
       libraryId,
@@ -27,22 +30,26 @@ const New = context => {
       writeToken,
       fileInfo,
       callback,
-      encryption: encrypt ? "cgck" : "none"
+      encryption: encrypt ? "cgck" : "none",
     });
   };
 
-  const callback = progress => {
-    Object.keys(progress).sort().forEach(filename => {
-      const {uploaded, total} = progress[filename];
-      const percentage = total === 0 ? "100.0%" : (100 * uploaded / total).toFixed(1) + "%";
-      context.concerns.Logger.log(`${filename}: ${percentage}`);
-    });
+  const callback = (progress) => {
+    Object.keys(progress)
+      .sort()
+      .forEach((filename) => {
+        const { uploaded, total } = progress[filename];
+        const percentage =
+          total === 0 ? "100.0%" : ((100 * uploaded) / total).toFixed(1) + "%";
+        context.concerns.Logger.log(`${filename}: ${percentage}`);
+      });
   };
 
-  const closeFileHandles = fileHandles => fileHandles.forEach(descriptor => fs.closeSync(descriptor));
+  const closeFileHandles = (fileHandles) =>
+    fileHandles.forEach((descriptor) => fs.closeSync(descriptor));
 
-  const fileInfo = fileHandles => {
-    return context.args.files.map(filePath => {
+  const fileInfo = (fileHandles) => {
+    return context.args.files.map((filePath) => {
       const fullPath = absPath(filePath, context.cwd);
       const fileDescriptor = fs.openSync(fullPath, "r");
       fileHandles.push(fileDescriptor);
@@ -50,16 +57,17 @@ const New = context => {
       const mimeType = mime.lookup(fullPath) || "video/mp4";
 
       return {
+        fullPath,
         path: path.basename(fullPath),
         type: "file",
         mime_type: mimeType,
         size: size,
-        data: fileDescriptor
+        data: fileDescriptor,
       };
     });
   };
 
-  return {add, callback, closeFileHandles, fileInfo};
+  return { add, callback, closeFileHandles, fileInfo };
 };
 
-module.exports = {blueprint, New};
+module.exports = { blueprint, New };
