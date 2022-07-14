1bbe0c58508a9dfd670424cb71d465a928a4ecd3
kevin.talmadge@eluv.io <kevin@eluv.io>
2022-07-14 11:34:49 -0700

    Increase timeout for popup close check

--- src/walletClient/Utils.js
+++ src/walletClient/Utils.js
@@ -218,7 +218,7 @@ exports.ActionPopup = async ({mode="tab", url, onMessage, onCancel}) => {
 
         resolve();
       }
-    }, 500);
+    }, 1000);
 
     window.addEventListener("message", async event => {
       await onMessage(
