3e3ac7001f334aa52a9ca13e85dbbd51e04704ea
jslching <38669839+jslching@users.noreply.github.com>
2022-09-01 16:18:10 -0400

    update xco sample

--- samples/auth/ClientSignedTokenXco.js
+++ samples/auth/ClientSignedTokenXco.js
@@ -10,25 +10,15 @@ const Utils = require("../../src/Utils");
 
 const networkName = "demo"; // "main" or "demo"
 
-const sampleXcMsg = {
-  chain: 'eip155:955305',
-  contract_addr: '0xd4c8153372b0292b364dac40d0ade37da4c4869a',
-  id: 1,
-  method: 'balanceOf',
-  params: {
-    owner: '0xcd8323da264e9c599af47a0d559dcdcb335d44ab'
-  }
-};
-
 const Setup = async () => {
 
   const signerURIs = ["https://host-216-66-89-94.contentfabric.io/as"];
 
   client = await ElvClient.FromNetworkName({networkName});
-  await client.SetRemoteSigner({signerURIs, idToken: process.env.PRIVATE_KEY, unsignedPublicAuth: true})
+  await client.SetRemoteSigner({signerURIs, idToken: process.env.ID_TOKEN, unsignedPublicAuth: true})
   client.ToggleLogging(false);
 
-  // Overwrite auth service endpoints (until the cross-chain feture is fully deployed)
+  // Overwrite auth service endpoints (until the cross-chain feature is fully deployed)
   //client.authServiceURIs = ["http://127.0.0.1:6546"];  // Dev instance
   client.authServiceURIs = signerURIs;
   client.AuthHttpClient.uris = signerURIs;
@@ -39,7 +29,7 @@ const Setup = async () => {
 /**
  * Make a cross-chain oracle call
  */
-const XcoMessage = async ({msg, usePersonal=false}) => {
+const XcoView = async ({msg, usePersonal=false}) => {
 
   // Create a client-signed-token in order to access the cross-chain oracle API
   const token = await client.CreateFabricToken({
@@ -91,52 +81,25 @@ const Run = async () => {
 
   client = await Setup();
 
-  console.log("Start using eth_sign")
-
-  // Call the oracle cross-chain 'view' API 'balanceOf'
-  let xcMsg = await XcoMessage({msg: sampleXcMsg});
-  console.log("XCO MSG", JSON.stringify(xcMsg));
-
-  // Create a client-signed-token including the 'xco-msg' as context
-  let accessToken = await client.CreateFabricToken({
-	  duration: 60 * 60 * 1000, // millisec
-    spec: {
-      ctx : {
-        xco_msg: xcMsg.xco_msg
-      }
+  const xcoReq = {
+    chain_type: 'eip155',
+    chain_id: '955305',
+    asset_type: 'erc721',
+    asset_id: '0xd4c8153372b0292b364dac40d0ade37da4c4869a',
+    method: 'balance',
+    params: {
+      owner: '0xcd8323da264e9c599af47a0d559dcdcb335d44ab'
     }
-  });
-  console.log("TOKEN", accessToken);
+  };
+
+  // Call the oracle cross-chain 'view' API 'balance'
+  let xcoResp = await XcoView({msg: xcoReq});
+  console.log("TOKEN", JSON.stringify(xcoResp));
+  accessToken = xcoResp.token;
 
   // Play
   let playoutOptions = await Play({token: accessToken});
   console.log("PLAYOUT", JSON.stringify(playoutOptions, null, 2));
-
-  console.log("Stop using eth_sign")
-
-  console.log("Start using personal_sign")
-
-  // Call the oracle cross-chain 'view' API 'balanceOf'
-  xcMsg = await XcoMessage({msg: sampleXcMsg, usePersonal: true});
-  console.log("XCO MSG", JSON.stringify(xcMsg));
-
-  // Create a client-signed-token including the 'xco-msg' as context
-  accessToken = await client.CreateFabricToken({
-    duration: 60 * 60 * 1000, // millisec
-    spec: {
-      ctx : {
-        xco_msg: xcMsg.xco_msg
-      }
-    },
-    addEthereumPrefix: false,
-  });
-  console.log("TOKEN", accessToken);
-
-  // Play
-  playoutOptions = await Play({token: accessToken});
-  console.log("PLAYOUT", JSON.stringify(playoutOptions, null, 2));
-
-  console.log("Stop using personal_sign")
 }
 
 if (!process.env.ID_TOKEN) {
