8de0052dcfdc1df5318993a507ff3bd37af270ee
elv-serban <serban@eluv.io>
2022-09-19 21:57:50 -0700

    Update auth samples to access a title meta and playout

--- samples/auth/ClientSignedToken.js
+++ samples/auth/ClientSignedToken.js
@@ -1,11 +1,40 @@
+/**
+ * ClientSignedToken
+ *
+ * Create a client-signed-token and access fabric resources.
+ *
+ * Using a private key:
+ *   - export PRIVATE_KEY - set to the private key of an account with
+ *                          access to the content object.
+ *
+ * Using a custodial wallet user:
+ *   - export ID_TOKEN - set to the JWT id token obtained from the
+ *                       open id sign in
+ *
+ */
 const { ElvClient } = require("../../src/ElvClient");
 
 const networkName = "demo"; // "main" or "demo"
 
-const Setup = async () => {
+const contentHash = "hq__93SK4rgxMarq1ZeDSEu9WJkDoptTKYiA2GmYocK7inMthUssGkG6Q9BREBEhNtVCiCBFsPd4Gd";
+
+const SetupWithCustodialWallet = async () => {
 
   client = await ElvClient.FromNetworkName({networkName});
   await client.SetRemoteSigner({idToken: process.env.ID_TOKEN, unsignedPublicAuth: true})
+  client.ToggleLogging(false);
+
+  return client;
+}
+
+const SetupWithPrivateKey = async () => {
+
+  client = await ElvClient.FromNetworkName({networkName});
+  let wallet = client.GenerateWallet();
+  let signer = wallet.AddAccount({
+    privateKey: process.env.PRIVATE_KEY
+  });
+  client.SetSigner({signer});
   client.ToggleLogging(false);
 
   return client;
@@ -15,6 +44,7 @@ const MakeClientSignedToken = async ({client}) => {
 
   const token = await client.CreateFabricToken({
 	  duration: 60 * 60 * 1000, // millisec
+    addEthereumPrefix: false,
   });
 
   console.log("ETH_TOKEN", token);
@@ -25,11 +55,34 @@ const MakeClientSignedTokenPersonal = async ({client}) => {
 
   const token = await client.CreateFabricToken({
     duration: 60 * 60 * 1000, // millisec
-    addEthereumPrefix: false,
+    addEthereumPrefix: true,
   });
 
   console.log("PERSONAL_TOKEN", token);
   return token;
+}
+
+/**
+ * Retrieve title metadata and playout URLs
+ */
+ const Play = async ({token}) => {
+
+  client.SetStaticToken({ token });
+
+  // First retrieve title metadata (title, synopsis, cast, ...)
+  let meta = await client.ContentObjectMetadata({
+    versionHash: contentHash,
+    metadataSubtree: "/public/asset_metadata"
+  });
+  console.log("META", meta);
+
+  // Retrieve playout info (DASH and HLS URLs)
+  let res = await client.PlayoutOptions({
+    versionHash: contentHash,
+    drms: ["clear", "aes-128", "fairplay", "widevine"]
+  });
+
+  return res;
 }
 
 const Run = async () => {
@@ -40,7 +93,9 @@ const Run = async () => {
   const dateNowStub = function () { return dateNow };
   global.Date.now = dateNowStub;
 
-  client = await Setup();
+  // Initialize client using either a private key or a custodial wallet id token
+  client = await SetupWithPrivateKey();
+  //client = await SetupWithCustodialWallet();
 
   const ethToken = await MakeClientSignedToken({client: client});
   const personalToken = await MakeClientSignedTokenPersonal({client: client});
@@ -53,11 +108,15 @@ const Run = async () => {
 
   // Restore Date.now()
   global.Date.now = realDateNow;
+
+  // Access content and play
+  let playoutOptions = await Play({token: ethToken});
+  console.log("PLAYOUT", JSON.stringify(playoutOptions, null, 2));
 }
 
-if (!process.env.ID_TOKEN) {
-  console.log("Must set environment variable ID_TOKEN");
-  exit;
+if (!process.env.ID_TOKEN && !process.env.PRIVATE_KEY) {
+  console.log("Must set environment variable PRIVATE_KEY or ID_TOKEN");
+  return;
 }
 
 Run();
--- samples/auth/ClientSignedTokenXco.js
+++ samples/auth/ClientSignedTokenXco.js
@@ -1,40 +1,102 @@
 /**
  * ClientSignedTokenXco
  *
- * Create a client-signed-token using a cross-chain oracle signed message (xco_msg) and
- * access fabric resources. Then, repeat using EIP-191 personal_sign.
+ * Use a cross-chain oracle signed message to access fabric resources.
+ *
+ * export PRIVATE_KEY - set to the private key of an account that owns
+ *                      a token (asset).
  */
 
 const { ElvClient } = require("../../src/ElvClient");
 const Utils = require("../../src/Utils");
 
+const Pako = require("pako");
+
 const networkName = "demo"; // "main" or "demo"
+
+const contentHash = "hq__93SK4rgxMarq1ZeDSEu9WJkDoptTKYiA2GmYocK7inMthUssGkG6Q9BREBEhNtVCiCBFsPd4Gd";
 
 const Setup = async () => {
 
-  // Overwrite auth service endpoints (until the cross-chain feature is fully deployed)
-  // const signerURIs = ["https://host-216-66-89-94.contentfabric.io/as"];
-
   client = await ElvClient.FromNetworkName({networkName});
-  await client.SetRemoteSigner({/*signerURIs,*/ idToken: process.env.ID_TOKEN, unsignedPublicAuth: true})
-  client.ToggleLogging(false);
 
-  // client.authServiceURIs = signerURIs;
-  // client.AuthHttpClient.uris = signerURIs;
+  let wallet = client.GenerateWallet();
+  let signer = wallet.AddAccount({
+    privateKey: process.env.PRIVATE_KEY
+  });
+  client.SetSigner({signer});
+
+  client.ToggleLogging(false);
 
   return client;
 }
 
 /**
+ * Create a simple local private key-based signer
+ */
+const LocalSigner = async () => {
+  tmpClient = await ElvClient.FromNetworkName({networkName});
+  let wallet = tmpClient.GenerateWallet();
+  let signer = wallet.AddAccount({
+    privateKey: process.env.PRIVATE_KEY
+  });
+  return signer;
+}
+
+/**
+ * Create a signed authorization token used for calling the cross-chain oracle
+ */
+ const CreateOracleAccessToken = async ({
+  duration,
+  contentSpaceId,
+  signer
+}) => {
+
+  const address = await signer.getAddress();
+
+  let token = {
+    sub:`iusr${Utils.AddressToHash(address)}`,
+    adr: Buffer.from(address.replace(/^0x/, ""), "hex").toString("base64"),
+    spc: contentSpaceId,
+    iat: Date.now(),
+    exp: Date.now() + duration,
+  };
+
+  let message = `Eluvio Content Fabric Access Token 1.0\n${JSON.stringify(token)}`;
+
+  const signature = await signer.signMessage(message);
+
+  const compressedToken = Pako.deflateRaw(Buffer.from(JSON.stringify(token), "utf-8"));
+  return `acspjc${Utils.B58(
+    Buffer.concat([
+      Buffer.from(signature.replace(/^0x/, ""), "hex"),
+      Buffer.from(compressedToken)
+    ])
+  )}`;
+}
+
+/**
  * Make a cross-chain oracle call
  */
-const XcoView = async ({msg, usePersonal=false}) => {
+ const XcoView = async ({contentSpaceId, signer}) => {
 
-  // Create a client-signed-token in order to access the cross-chain oracle API
-  const token = await client.CreateFabricToken({
-	  duration: 60 * 60 * 1000, // millisec
-    addEthereumPrefix: !usePersonal,
+  // Create a client-signed access token  in order to access the cross-chain oracle API
+  let xcoToken = await CreateOracleAccessToken({
+    duration: 1 * 1000, // millisec
+    contentSpaceId: contentSpaceId,
+    address: signer.getAddress(),
+    signer: client.signer
   });
+  console.log("ORACLE ACCESS TOKEN", xcoToken);
+
+  // Format cross-chain oracle request
+  const xcoReq = {
+    chain_type: 'eip155',
+    chain_id: '955210',
+    asset_type: 'erc721',
+    asset_id: '0xc21ea77699666e2bb6b96dd20157db08f22cb9c3',
+    method: 'balance'
+  };
 
   // Call the cross-chain oracle 'view' API
   let res = await Utils.ResponseToFormat(
@@ -42,9 +104,9 @@ const XcoView = async ({msg, usePersonal=false}) => {
     client.authClient.MakeAuthServiceRequest({
       method: "POST",
       path: "/as/xco/view",  // On main/dev net /as/xco/view
-      body: msg,
+      body: xcoReq,
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${xcoToken}`,
       },
     })
   );
@@ -55,10 +117,7 @@ const XcoView = async ({msg, usePersonal=false}) => {
 /**
  * Retrieve playout URLs
  */
-const Play = async ({token}) => {
-  const contentHash = "hq__93SK4rgxMarq1ZeDSEu9WJkDoptTKYiA2GmYocK7inMthUssGkG6Q9BREBEhNtVCiCBFsPd4Gd";
-
-  client.SetStaticToken({ token });
+const Play = async ({}) => {
 
   // First retrieve title metadata (title, synopsis, cast, ...)
   let meta = await client.ContentObjectMetadata({
@@ -78,32 +137,32 @@ const Play = async ({token}) => {
 
 const Run = async () => {
 
-  client = await Setup();
+  let client = await Setup();
 
-  const xcoReq = {
-    chain_type: 'eip155',
-    chain_id: '955210',
-    asset_type: 'erc721',
-    asset_id: '0xc21ea77699666e2bb6b96dd20157db08f22cb9c3',
-    method: 'balance',
-    params: {
-      owner: '0x9d4a49b2b6aff4ae42767d95664cbd948d89e0c1'
-    }
-  };
+  // Set custom signer here (for example a web3 metamask signer)
+  // This example just uses a simple private key signer
+  let signer = await LocalSigner();
 
   // Call the oracle cross-chain 'view' API 'balance'
-  let xcoResp = await XcoView({msg: xcoReq});
-  console.log("TOKEN", JSON.stringify(xcoResp));
+  let xcoResp = await XcoView({
+    contentSpaceId: client.ContentSpaceId(),
+    signer: signer
+  });
+  console.log("FABRIC ACCESS TOKEN", JSON.stringify(xcoResp));
   accessToken = xcoResp.token;
 
+  // Create a client object and use the access token returned by the
+  // cross-chain oracle.
+  client.SetStaticToken({ token: accessToken });
+
   // Play
-  let playoutOptions = await Play({token: accessToken});
+  let playoutOptions = await Play({});
   console.log("PLAYOUT", JSON.stringify(playoutOptions, null, 2));
 }
 
-if (!process.env.ID_TOKEN) {
-  console.log("Must set environment variable ID_TOKEN");
-  exit;
+if (!process.env.PRIVATE_KEY) {
+  console.log("Must set environment variable PRIVATE_KEY");
+  return;
 }
 
 Run();
