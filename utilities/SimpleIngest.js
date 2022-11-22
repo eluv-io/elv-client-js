5fb812d390e80f7616b2e7d1d44cdfc126fbe51c
Todd Hodes <108554594+elv-todd@users.noreply.github.com>
2022-11-22 13:52:06 -0800

    Add cross-chain oracle extensions (#186)
    - add src/CrossChainOracle.js
    - apply PR https://github.com/eluv-io/elv-client-js/pull/154 -- MetaMask doesn't have sign digest method

--- utilities/SimpleIngest.js
+++ utilities/SimpleIngest.js
@@ -279,6 +279,14 @@ class SimpleIngest extends Utility {
   }
 }
 
+const originalEmit = process.emit;
+process.emit = function (name, data, ...args) {
+  if(name === `warning` && typeof data === `object` && data.name === `ExperimentalWarning`) {
+    return false;
+  }
+  return originalEmit.apply(process, arguments);
+};
+
 if(require.main === module) {
   Utility.cmdLineInvoke(SimpleIngest);
 } else {
