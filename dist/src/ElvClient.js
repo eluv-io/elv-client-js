03ff0fc9e700db362393437d8fabd051474682a4
elv-zenia <zenia.villa@eluv.io>
2023-01-17 14:25:41 -0800

    Update build

--- dist/src/ElvClient.js
+++ dist/src/ElvClient.js
@@ -143,8 +143,8 @@ var ElvClient = /*#__PURE__*/function () {
     this.noAuth = noAuth;
     this.assumeV3 = assumeV3;
 
-    if (!["search", "default"].includes(this.service)) {
-      throw Error("Invalid service: ".concat(this.service));
+    if (!["search", "default"].includes(service)) {
+      throw Error("Invalid service: ".concat(service));
     }
 
     this.service = service;
