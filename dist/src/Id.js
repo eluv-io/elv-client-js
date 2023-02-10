cb2205e1f53bbb77c2038391c3bc837748494b8f
kevin.talmadge@eluv.io <kevin@eluv.io>
2023-02-09 22:17:15 -0800

    Update build

--- dist/src/Id.js
+++ dist/src/Id.js
@@ -1,14 +1,17 @@
 var _classCallCheck = require("@babel/runtime/helpers/classCallCheck");
-var _createClass = require("@babel/runtime/helpers/createClass");
-// Incremental numerical IDs
 
+var _createClass = require("@babel/runtime/helpers/createClass");
+
+// Incremental numerical IDs
 var __id = 0;
+
 var Id = /*#__PURE__*/function () {
   "use strict";
 
   function Id() {
     _classCallCheck(this, Id);
   }
+
   _createClass(Id, null, [{
     key: "next",
     value: function next() {
@@ -16,6 +19,8 @@ var Id = /*#__PURE__*/function () {
       return __id;
     }
   }]);
+
   return Id;
 }();
+
 module.exports = Id;
