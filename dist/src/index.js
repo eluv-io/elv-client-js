06a57e8b8075eef38259f302321c6e8e25a2b4f1
kevin.talmadge@eluv.io <kevin@eluv.io>
2023-02-14 12:53:37 -0800

    Fix contract operation in remote signer ; Update dependencies ; Remove build from git

--- dist/src/index.js
+++
@@ -1,8 +0,0 @@
-var _require = require("./ElvClient.js"),
-  ElvClient = _require.ElvClient;
-var _require2 = require("./walletClient/index.js"),
-  ElvWalletClient = _require2.ElvWalletClient;
-var Utils = require("./Utils.js");
-exports.ElvClient = ElvClient;
-exports.ElvWalletClient = ElvWalletClient;
-exports.Utils = Utils;
