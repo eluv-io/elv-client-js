c669ebc1d1b7abceae12f8fcdf1e39488d83b900
elv-zenia <zenia.villa@eluv.io>
2023-05-25 14:47:11 -0700

    Add searchable_links to defaultSpec

--- typeSpecs/Default.js
+++ typeSpecs/Default.js
@@ -74,6 +74,12 @@ const defaultSpec = {
       defaultable: false,
       orderable: true
     }
+  ],
+  searchable_links: [
+    {target: "/public/asset_metadata", link_key: "asset_metadata"},
+    {target: "/public/assets", link_key: "assets"},
+    {target: "/offerings", link_key: "offerings"},
+    {target: "/video_tags", link_key: "video_tags"}
   ]
 };
 
