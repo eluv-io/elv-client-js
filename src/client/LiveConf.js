f4f413782b0fb6bb0059fd0f1c64e8b3152c2a52
elv-serban <serban@eluv.io>
2024-03-19 16:55:53 -0700

    Fix RTMP fps 24 and 48 correctly this time

--- src/client/LiveConf.js
+++ src/client/LiveConf.js
@@ -282,7 +282,7 @@ class LiveConf {
     switch(frameRate) {
       case "24":
         seg.videoTimeBase = 1536; // Output timebase: 12288
-        seg.videoFrameDurationTs = 640;
+        seg.videoFrameDurationTs = 512;
         seg.video = this.calcOutputTimebase(seg.videoTimeBase) * 30;
         seg.keyint = 48;
         seg.duration = "30";
@@ -306,7 +306,7 @@ class LiveConf {
         break;
       case "48":
         seg.videoTimeBase = 1536; // Output timebase: 12288
-        seg.videoFrameDurationTs = 320;
+        seg.videoFrameDurationTs = 256;
         seg.video = this.calcOutputTimebase(seg.videoTimeBase) * 30;
         seg.keyint = 96;
         seg.duration = "30";
