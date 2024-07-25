66abc762dbe4f97b891757812ce1a029ec399729
elv-preethi <39742400+elv-preethi@users.noreply.github.com>
2024-07-24 23:31:22 -0700

    make changes as per comment

--- testScripts/OfferingDownloadMedia.js
+++ testScripts/OfferingDownloadMedia.js
@@ -5,17 +5,20 @@ const Fraction = require("fraction.js");
 const {JSONPath} = require("jsonpath-plus");
 const fs = require("fs");
 const path = require("path");
+const { execSync } = require("child_process");
 
-class ObjectDownloadMedia extends ScriptBase {
+class OfferingDownloadMedia extends ScriptBase {
   async body() {
     const client = await this.client();
 
     let objectId = this.args.objectId;
-    const versionHash = this.args.objectHash;
-    const offeringType = this.args.offeringType || "default";
+    const versionHash = this.args.versionHash;
+    const offeringKey = this.args.offeringKey;
+    const streamKey = this.args.streamKey;
     const startTime = this.args.startTime;
     const endTime = this.args.endTime;
-    const out = this.args.out || "./out";
+    const out = this.args.out;
+
 
     if(objectId === undefined){
       if(versionHash===undefined){
@@ -26,7 +29,7 @@ class ObjectDownloadMedia extends ScriptBase {
     }
 
     const dirPath = path.resolve(out);
-    if (!fs.existsSync(dirPath)) {
+    if(!fs.existsSync(dirPath)) {
       fs.mkdirSync(dirPath, { recursive: true });
       console.log(`Directory created at ${dirPath}`);
     } else {
@@ -34,9 +37,9 @@ class ObjectDownloadMedia extends ScriptBase {
     }
 
 
-    const sourcesJsonPath = "offerings." + offeringType + ".media_struct.streams.video.sources[*]";
+    const sourcesJsonPath = "offerings." + offeringKey + ".media_struct.streams.video.sources[*]";
     const libraryId = await client.ContentObjectLibraryId({objectId, versionHash});
-    const totalDurationJsonPath = "offerings." + offeringType + ".media_struct.streams.video.duration";
+    const totalDurationJsonPath = "offerings." + offeringKey + ".media_struct.streams." + streamKey + ".duration";
 
     // get object metadata
     let metadata = await client.ContentObjectMetadata({
@@ -67,19 +70,22 @@ class ObjectDownloadMedia extends ScriptBase {
     if(startTime < 0 || endTime > totalDuration) {
       throw new Error(`start or end time provided are not within range: ${totalDuration}s`);
     }
+    if(startTime === endTime) {
+      throw new Error(`end time needs to be greater than start time, value provided: ${startTime}s`);
+    }
 
-    const sourcesTimeInfo = sourcesMetadata.map((item) => {
-      // start = item.timeline_start.ts * item.timeline_start.time_base
-      const startTimeBase = new Fraction(item.timeline_start.time_base);
-      const start = new Fraction(item.timeline_start.ts).mul(startTimeBase);
-      // end = item.timeline_start.ts * item.timeline_end.time_base
-      const endTimeBase = new Fraction(item.timeline_end.time_base);
-      const end = new Fraction(item.timeline_end.ts).mul(endTimeBase);
+    const sourcesTimeInfo = sourcesMetadata.map((part) => {
+      // start = part.timeline_start.ts * part.timeline_start.time_base
+      const startTimeBase = new Fraction(part.timeline_start.time_base);
+      const start = new Fraction(part.timeline_start.ts).mul(startTimeBase);
+      // end = part.timeline_start.ts * part.timeline_end.time_base
+      const endTimeBase = new Fraction(part.timeline_end.time_base);
+      const end = new Fraction(part.timeline_end.ts).mul(endTimeBase);
 
       return {
         start: start.valueOf(),
         end: end.valueOf(),
-        source: item.source
+        source: part.source
       };
     });
 
@@ -94,17 +100,43 @@ class ObjectDownloadMedia extends ScriptBase {
     console.log(parts);
     console.log();
 
+
+    let mtpath = path.join(dirPath, streamKey);
+    if(!fs.existsSync(mtpath)) {
+      fs.mkdirSync(mtpath, { recursive: true });
+      console.log(`Directory created at ${mtpath}`);
+    }
+    let partsfile = path.join(dirPath, "/parts_" + streamKey + ".txt");
+
     console.log("Downloading parts...");
     for(const partHash of parts) {
       console.log(partHash);
-      const arrayBuffer = await client.DownloadPart({
+      const buf = await client.DownloadPart({
         libraryId,
         objectId,
-        partHash
+        partHash,
+        format: "buffer",
+        chunked: false,
+        callback: ({bytesFinished, bytesTotal}) => {
+          console.log("  progress: ", bytesFinished + "/" + bytesTotal);
+        }
       });
-      let filePath = path.join(dirPath, partHash);
-      fs.writeFileSync(filePath, Buffer.from(arrayBuffer));
+
+      let partFile = path.join(mtpath, partHash + ".mp4");
+      fs.appendFile(partFile, buf, (err) => {
+        if(err)
+          console.log(err);
+      });
+      fs.appendFile(partsfile, "file '" + streamKey + "/" + partHash + ".mp4'\n", (err) => {
+        if(err)
+          console.log(err);
+      });
     }
+
+    // Concatenate parts into one mp4
+    let cmd = "ffmpeg -f concat -safe 0 -i " + partsfile + " -c copy " + dirPath + "/" + streamKey + ".mp4";
+    console.log("Running", cmd);
+    execSync(cmd);
   }
 
   header() {
@@ -118,29 +150,38 @@ class ObjectDownloadMedia extends ScriptBase {
         describe: "Object ID (should start with 'iq__')",
         type: "string"
       })
-      .option("objectHash", {
-        alias: "object-hash",
-        describe: "Object Hash (should start with 'hq__')",
+      .option("versionHash", {
+        alias: "version-hash",
+        describe: "Object Version Hash (should start with 'hq__')",
         type: "string"
       })
-      .option("offeringType", {
-        describe: "offerings type (default: 'default')",
+      .option("offeringKey", {
+        describe: "offering key",
         type: "string",
+        default: "default",
+      })
+      .option("streamKey", {
+        describe: "offerings stream key: audio/video",
+        type: "string",
+        default: "video",
       })
       .option("startTime", {
         describe: "start time to retrieve parts",
-        type: "number"
+        demandOption: true,
+        type: "number",
       })
       .option("endTime", {
         describe: "end time to retrieve parts",
-        type: "number"
+        demandOption: true,
+        type: "number",
       })
       .option("out",{
         describe: "output directory",
-        type: "string"
+        type: "string",
+        default: "./out",
       });
   }
 }
 
-const script = new ObjectDownloadMedia();
+const script = new OfferingDownloadMedia();
 script.run();
