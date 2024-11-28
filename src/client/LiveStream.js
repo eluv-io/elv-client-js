0e89092b9fb90108b0da1bb0848764cad6ab153a
elv-zenia <zenia.villa@eluv.io>
2024-11-27 19:42:34 -0800

    Refine

--- src/client/LiveStream.js
+++ src/client/LiveStream.js
@@ -518,12 +518,10 @@ exports.StreamStatus = async function({name, stopLro=false, showParams=false}) {
 
     const segDurationMeta = edgeMeta.live_recording.recording_config.recording_params.xc_params.seg_duration
 
-    const segDuration = segDurationMeta ? (parseInt(segDurationMeta) + 5) : null;
-
     // Convert LRO 'state' to desired 'state'
     if(state === "running" && videoLastFinalizationTimeEpochSec <= 0) {
       state = "starting";
-    } else if(state === "running" && segDuration && sinceLastFinalize > segDuration) {
+    } else if(state === "running" && segDurationMeta !== undefined && sinceLastFinalize > (parseInt(segDurationMeta) + 5)) {
       state = "stalled";
     } else if(state == "terminated") {
       state = "stopped";
