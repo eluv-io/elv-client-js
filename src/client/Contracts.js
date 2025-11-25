f1dceaa9b4fcdaf75a1390c5785ddaaf9ddc4c18
elv-preethi <preethi.manoharan@eluv.io>
2025-11-25 13:18:20 -0800

    add retrieving tenant groups

--- src/client/Contracts.js
+++ src/client/Contracts.js
@@ -928,6 +928,93 @@ exports.ResetTenantId = async function({contractAddress, objectId, versionHash})
   }
 };
 
+async function getTenantGroupAddress({ctx, tenantContractId, groupName}) {
+  if(!tenantContractId) {
+    throw new Error("tenantContractId is required");
+  }
+
+  if(!tenantContractId.startsWith("iten")) {
+    throw new Error(`Invalid Tenant Contract ID format: ${tenantContractId}`);
+  }
+
+  const tenantAddress = Utils.HashToAddress(tenantContractId);
+
+  // Needs to be tenant version
+  const version = await ctx.authClient.AccessType(tenantContractId);
+  if(version !== ctx.authClient.ACCESS_TYPES.TENANT) {
+    throw new Error(
+      `Contract ${tenantContractId} is not a Tenant. Actual version: ${version}`
+    );
+  }
+
+  try {
+    const groupAddress = await ctx.CallContractMethod({
+      contractAddress: tenantAddress,
+      methodName: "groupsMapping",
+      methodArgs: [groupName, 0],
+      formatArguments: true,
+    });
+
+    if (!groupAddress) {
+      throw new Error(`${groupName} group address not set.`);
+    }
+
+    return groupAddress;
+  } catch (err) {
+    this.Log(err);
+    throw new Error(`Failed to retrieve ${groupName} group address.`);
+  }
+
+}
+
+/**
+ * Returns the tenant_admin group address for a given tenant contract.
+ *
+ * @methodGroup Tenant
+ * @namedParams
+ * @param {string=} tenantContractId - The ID of the tenant contract
+ * @returns {Promise<*|undefined>}
+ */
+exports.TenantAdminGroup = async function({ tenantContractId }) {
+  return getTenantGroupAddress({
+    ctx : this,
+    tenantContractId,
+    groupName: "tenant_admin"
+  });
+};
+
+/**
+ * Returns the content_admin group address for a given tenant contract.
+ *
+ * @methodGroup Tenant
+ * @namedParams
+ * @param {string=} tenantContractId - The ID of the tenant contract
+ * @returns {Promise<*|undefined>}
+ */
+exports.ContentAdminGroup = async function({tenantContractId}){
+  return getTenantGroupAddress({
+    ctx : this,
+    tenantContractId,
+    groupName: "content_admin"
+  });
+};
+
+/**
+ * Returns the tenant_users group address for a given tenant contract.
+ *
+ * @methodGroup Tenant
+ * @namedParams
+ * @param {string=} tenantContractId - The ID of the tenant contract
+ * @returns {Promise<*|undefined>}
+ */
+exports.TenantUsersGroup = async function({tenantContractId}){
+  return getTenantGroupAddress({
+    ctx : this,
+    tenantContractId,
+    groupName: "tenant_users"
+  });
+};
+
 /**
  * Enum for object types that can be cleaned up after object deletion.
  * Used by the ObjectCleanup method to determine which associated objects to clean.
