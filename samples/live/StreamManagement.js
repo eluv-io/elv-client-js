833466bb563c0359067cf98121dfc779b43857c1
Zenia Villa <zenia.villa@eluv.io>
2026-01-08 13:08:02 -0800

    Update drm in CreateStream sample

--- samples/live/StreamManagement.js
+++ samples/live/StreamManagement.js
@@ -4,7 +4,7 @@ let client;
 const networkName = "demov3"; // "main" or "demo"
 const libraryId = "ilib3xjQjKgtB1d1aKwEwaB1hUioYFar";
 const accessGroups = [];
-const url = "udp://host-76-74-28-233.contentfabric.io:11051";
+const url = "";
 
 const Setup = async() => {
   client = await ElvClient.FromNetworkName({networkName});
@@ -22,86 +22,88 @@ const CreateStream = async() => {
     libraryId,
     url,
     options: {
-      name: "Z - Live Stream - 11/01/25 Concert Tour",
+      name: "Live Stream - 11/01/25 Concert Tour",
       displayTitle: "Farewell Tour - 11/01/25",
       description: "The last night of the 11/01/25 Farewell Tour",
       accessGroups,
       linkToSite: false
     },
     liveRecordingConfig: {
-    //   profile: "M6",
-    //   drm_type: "drm-all", // drm-public | drm-all | drm-fairplay | drm-widevine | drm-playready | clear
-    //   recording_config: {
-    //     part_ttl: 3600, // seconds
-    //     reconnect_timeout: 1800, // seconds
-    //     connection_timeout: 1800, // seconds
-    //     copy_mpeg_ts: true,
-    //     input_cfg: {
-    //       bypass_libav_reader: true,
-    //       copy_mode: "raw",
-    //       copy_packaging: "rtp_ts",
-    //       packaging_mode: "rtp_ts",
-    //     }
-    //   },
-    //   // recording_stream_config: {
-    //   //   audio: {
-    //   //     1: {
-    //   //       "bitrate": 265988,
-    //   //       "codec": "aac",
-    //   //       "default": true,
-    //   //       "lang": "en-us",
-    //   //       "playout": true,
-    //   //       "playout_label": "Audio 1",
-    //   //       "record": true,
-    //   //       "recording_bitrate": 192000,
-    //   //       "recording_channels": 2
-    //   //     },
-    //   //     2: {
-    //   //       "bitrate": 265988,
-    //   //       "codec": "aac",
-    //   //       "default": false,
-    //   //       "lang": "fr-fr",
-    //   //       "playout": true,
-    //   //       "playout_label": "Audio 2",
-    //   //       "record": true,
-    //   //       "recording_bitrate": 192000,
-    //   //       "recording_channels": 2
-    //   //     },
-    //   //     3: {
-    //   //       "bitrate": 265988,
-    //   //       "codec": "aac",
-    //   //       "lang": "fr-ca",
-    //   //       "default": false,
-    //   //       "playout": true,
-    //   //       "playout_label": "Audio 3",
-    //   //       "record": true,
-    //   //       "recording_bitrate": 192000,
-    //   //       "recording_channels": 2
-    //   //     },
-    //   //     4: {
-    //   //       "bitrate": 265988,
-    //   //       "codec": "aac",
-    //   //       "lang": "es-es",
-    //   //       "default": false,
-    //   //       "playout": true,
-    //   //       "playout_label": "Audio 4",
-    //   //       "record": true,
-    //   //       "recording_bitrate": 192000,
-    //   //       "recording_channels": 2
-    //   //     },
-    //   //     5: {
-    //   //       "bitrate": 265988,
-    //   //       "codec": "aac",
-    //   //       "lang": "de-de",
-    //   //       "default": false,
-    //   //       "playout": true,
-    //   //       "playout_label": "Audio 5",
-    //   //       "record": true,
-    //   //       "recording_bitrate": 192000,
-    //   //       "recording_channels": 2
-    //   //     }
-    //   //   }
-    //   // },
+      profile: "M6",
+      playout_config: {
+        drm: "drm-all" // drm-public | drm-all | drm-fairplay | drm-widevine | drm-playready | clear
+      },
+      recording_config: {
+        part_ttl: 3600, // seconds
+        reconnect_timeout: 1800, // seconds
+        connection_timeout: 1800, // seconds
+        copy_mpeg_ts: true,
+        input_cfg: {
+          bypass_libav_reader: true,
+          copy_mode: "raw",
+          copy_packaging: "rtp_ts",
+          packaging_mode: "rtp_ts",
+        }
+      },
+      recording_stream_config: {
+        audio: {
+          1: {
+            "bitrate": 265988,
+            "codec": "aac",
+            "default": true,
+            "lang": "en-us",
+            "playout": true,
+            "playout_label": "Audio 1",
+            "record": true,
+            "recording_bitrate": 192000,
+            "recording_channels": 2
+          },
+          2: {
+            "bitrate": 265988,
+            "codec": "aac",
+            "default": false,
+            "lang": "fr-fr",
+            "playout": true,
+            "playout_label": "Audio 2",
+            "record": true,
+            "recording_bitrate": 192000,
+            "recording_channels": 2
+          },
+          3: {
+            "bitrate": 265988,
+            "codec": "aac",
+            "lang": "fr-ca",
+            "default": false,
+            "playout": true,
+            "playout_label": "Audio 3",
+            "record": true,
+            "recording_bitrate": 192000,
+            "recording_channels": 2
+          },
+          4: {
+            "bitrate": 265988,
+            "codec": "aac",
+            "lang": "es-es",
+            "default": false,
+            "playout": true,
+            "playout_label": "Audio 4",
+            "record": true,
+            "recording_bitrate": 192000,
+            "recording_channels": 2
+          },
+          5: {
+            "bitrate": 265988,
+            "codec": "aac",
+            "lang": "de-de",
+            "default": false,
+            "playout": true,
+            "playout_label": "Audio 5",
+            "record": true,
+            "recording_bitrate": 192000,
+            "recording_channels": 2
+          }
+        }
+      },
       input_stream_info: {
         format: {
           format_name: "mpegts"
@@ -132,46 +134,46 @@ const CreateStream = async() => {
             stream_index: 1,
             time_base: "1/90000"
           },
-          // {
-          //   codec_name: "aac",
-          //   codec_type: "audio",
-          //   channel_layout: 3,
-          //   channels: 2,
-          //   sample_rate: 48000,
-          //   stream_id: 112,
-          //   stream_index: 2,
-          //   time_base: "1/90000"
-          // },
-          // {
-          //   codec_name: "aac",
-          //   codec_type: "audio",
-          //   channel_layout: 3,
-          //   channels: 2,
-          //   sample_rate: 48000,
-          //   stream_id: 113,
-          //   stream_index: 3,
-          //   time_base: "1/90000"
-          // },
-          // {
-          //   codec_name: "aac",
-          //   codec_type: "audio",
-          //   channel_layout: 3,
-          //   channels: 2,
-          //   sample_rate: 48000,
-          //   stream_id: 114,
-          //   stream_index: 4,
-          //   time_base: "1/90000"
-          // },
-          // {
-          //   codec_name: "aac",
-          //   codec_type: "audio",
-          //   channel_layout: 3,
-          //   channels: 2,
-          //   sample_rate: 48000,
-          //   stream_id: 115,
-          //   stream_index: 5,
-          //   time_base: "1/90000"
-          // }
+          {
+            codec_name: "aac",
+            codec_type: "audio",
+            channel_layout: 3,
+            channels: 2,
+            sample_rate: 48000,
+            stream_id: 112,
+            stream_index: 2,
+            time_base: "1/90000"
+          },
+          {
+            codec_name: "aac",
+            codec_type: "audio",
+            channel_layout: 3,
+            channels: 2,
+            sample_rate: 48000,
+            stream_id: 113,
+            stream_index: 3,
+            time_base: "1/90000"
+          },
+          {
+            codec_name: "aac",
+            codec_type: "audio",
+            channel_layout: 3,
+            channels: 2,
+            sample_rate: 48000,
+            stream_id: 114,
+            stream_index: 4,
+            time_base: "1/90000"
+          },
+          {
+            codec_name: "aac",
+            codec_type: "audio",
+            channel_layout: 3,
+            channels: 2,
+            sample_rate: 48000,
+            stream_id: 115,
+            stream_index: 5,
+            time_base: "1/90000"
+          }
         ]
       }
     }
