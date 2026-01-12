1f6fdc90c42b894e7e67111769d3a6b17930b7b1
Zenia Villa <zenia.villa@eluv.io>
2026-01-12 13:31:24 -0800

    Missing hls playready cenc from drm-public

--- src/client/LiveStream.js
+++ src/client/LiveStream.js
@@ -30,7 +30,7 @@ const Sleep = (ms) => {
 };
 
 const ENCRYPTION_OPTIONS = [
-  {value: "drm-public", format: ["hls-sample-aes", "hls-aes128", "dash-widevine"], id: "drm-public"},
+  {value: "drm-public", format: ["hls-sample-aes", "hls-aes128", "dash-widevine", "hls-playready-cenc"], id: "drm-public"},
   {value: "drm-all",  format: ["hls-sample-aes", "hls-aes128", "hls-fairplay", "hls-widevine-cenc", "hls-playready-cenc", "dash-widevine"], id: "drm-all"},
   {value: "drm-fairplay", format: ["hls-fairplay"], id: "drm-fairplay"},
   {value: "drm-widevine", format: ["hls-widevine-cenc", "dash-widevine"], id: "drm-widevine"},
