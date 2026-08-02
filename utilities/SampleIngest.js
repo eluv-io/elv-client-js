b5522f8eea348e7f32e3e0c3790f0aeb4fe87c80
Serban Simu <serban@eluv.io>
2026-08-02 00:38:52 -0700

    Revert unrelated changes

--- utilities/SampleIngest.js
+++ utilities/SampleIngest.js
@@ -1,6 +1,5 @@
 // Create new production master from specified file(s)
 const R = require("ramda");
-const fs = require("fs");
 
 const {ModOpt, NewOpt} = require("./lib/options");
 const Utility = require("./lib/Utility");
@@ -8,69 +7,29 @@ const Utility = require("./lib/Utility");
 const ABR = require("@eluvio/elv-abr-profile");
 
 const Client = require("./lib/concerns/Client");
-const CloudFile = require("./lib/concerns/CloudFile");
 const Finalize = require("./lib/concerns/Finalize");
 const LocalFile = require("./lib/concerns/LocalFile");
 const LRO = require("./lib/concerns/LRO");
 const ArgLibraryId = require("./lib/concerns/ArgLibraryId");
 const ArgTenant = require("./lib/concerns/ArgTenant");
 const {seconds} = require("./lib/helpers");
-
 const AbrProfile = require("./lib/abr_profiles/abr_profile_clear.json");
-const STATUS_FILE = "./ingest-status.json";
-
-// Status object - used for reporting and resume.
-const status = {
-  state: "",
-  object_id: "",
-  write_token: "",
-  master_created: false
-};
-
-const SetStatus = statusUpdate => {
-  Object.assign(status, statusUpdate);
-  fs.writeFileSync(STATUS_FILE, `${JSON.stringify(status, null, 2)}\n`);
-};
 
 class SampleIngest extends Utility {
   blueprint() {
     return {
-      concerns: [Client, CloudFile, Finalize, LocalFile, ArgLibraryId, ArgTenant, LRO],
+      concerns: [Client, Finalize, LocalFile, ArgLibraryId, ArgTenant, LRO],
       options: [
-        ModOpt("libraryId", {demand: false, forX: "new media object"}),
-        NewOpt("objectId", {
-          demand: false,
-          descTemplate: "Existing media object to ingest into",
-          type: "string"
-        }),
+        ModOpt("libraryId", {demand: true, forX: "new media object"}),
         NewOpt("title", {
-          demand: false,
-          descTemplate: "Title for new media object (required unless --object-id is given)",
+          demand: true,
+          descTemplate: "Title for new media object",
           type: "string"
         }),
         NewOpt("drm", {
           default: false,
           descTemplate: "Use DRM for playback",
           type: "boolean"
-        }),
-        NewOpt("hdrInfoFile", {
-          descTemplate: "Path to HDR info JSON file to enable HDR10 ingest. File format: { \"master_display\": \"G(...)B(...)R(...)WP(...)L(...)\", \"max_cll\": \"...,...\" }",
-          type: "string"
-        }),
-        NewOpt("finalize", {
-          default: true,
-          descTemplate: "Finalize the content object write token after ingest. Use --no-finalize to leave the draft open.",
-          type: "boolean"
-        }),
-        NewOpt("offeringKey", {
-          default: "default",
-          descTemplate: "Offering key (default: 'default')",
-          type: "string"
-        }),
-        NewOpt("variantKey", {
-          default: "default",
-          descTemplate: "Master variant key (default: 'default')",
-          type: "string"
         }),
         ModOpt("files", {forX: "for new media object"})
       ]
@@ -80,19 +39,8 @@ class SampleIngest extends Utility {
   async body() {
     const logger = this.logger;
 
-    const s3Reference = Boolean(this.args.s3Reference);
-    const useS3 = Boolean(this.args.s3Copy || s3Reference);
-    const access = useS3
-      ? this.concerns.CloudFile.credentialSet()
-      : undefined;
-
     let fileHandles = [];
-    const fileInfo = useS3
-      ? this.concerns.CloudFile.fileInfo()
-      : this.concerns.LocalFile.fileInfo(fileHandles);
-    const fileConcern = useS3
-      ? this.concerns.CloudFile
-      : this.concerns.LocalFile;
+    const fileInfo = this.concerns.LocalFile.fileInfo(fileHandles);
 
     // delay getting elvClient until this point so script exits faster
     // if there is a validation error above
@@ -105,113 +53,46 @@ class SampleIngest extends Utility {
 
     if(R.isNil(type)) throw Error("Library does not specify content type for sample ingests");
 
-    let {drm, finalize, hdrInfoFile, libraryId, objectId, offeringKey, title, variantKey} = this.args;
-    const noFinalize = !finalize;
-    const copy = !s3Reference;
-    const encrypt = !s3Reference;
+    const {drm, libraryId, title} = this.args;
+    const encrypt = true;
 
-    let id;
-    let nodeUrl;
-    let writeToken;
-    if(status.write_token) {
-      id = status.object_id;
-      writeToken = status.write_token;
-      if(!libraryId) {
-        libraryId = await client.ContentObjectLibraryId({objectId: id});
-      }
-      // Re-pin client to the node holding the draft
-      nodeUrl = await client.WriteTokenNodeUrlNetwork({writeToken});
-      await client.SetNodes({fabricURIs: [nodeUrl]});
-      logger.log(`Resuming from write token ${writeToken} on ${nodeUrl}`);
-    } else {
-      if(!libraryId && !objectId) {
-        throw Error("One of library-id or object-id must be specified");
-      }
+    const {id, writeToken} = await client.CreateContentObject({
+      libraryId,
+      options: type ? { type } : {}
+    });
 
-      if(!objectId && !title) {
-        throw Error("--title is required when creating a new media object");
-      }
+    logger.log("Uploading files...");
 
-      if(objectId) {
-        if(!libraryId) {
-          libraryId = await client.ContentObjectLibraryId({objectId});
-        }
-        const editResp = await client.EditContentObject({
-          libraryId,
-          objectId,
-          options: type ? { type } : {}
-        });
-        id = objectId;
-        writeToken = editResp.writeToken;
-        nodeUrl = editResp.nodeUrl;
-      } else {
-        const createResp = await client.CreateContentObject({
-          libraryId,
-          options: type ? { type } : {}
-        });
-        id = createResp.id;
-        writeToken = createResp.writeToken;
-        nodeUrl = createResp.nodeUrl;
-      }
+    const createMasterResponse = await client.CreateProductionMaster({
+      libraryId,
+      type,
+      name: title,
+      description: `Media object created via sample ingest: ${title}`,
+      fileInfo,
+      encrypt,
+      copy: true,
+      callback: this.concerns.LocalFile.callback,
+      writeToken
+    });
 
-      await client.authClient.GenerateAuthorizationToken({
-        libraryId,
-        objectId: id,
-        update: true
-      });
-
-      status.object_id = id;
-      status.write_token = writeToken;
-    }
-
-    SetStatus({state: "object_created"});
-
-    if(!nodeUrl) {
-      nodeUrl = client.WriteTokenNodeUrlLocal({writeToken});
-    }
-    if(!nodeUrl) {
-      nodeUrl = await client.WriteTokenNodeUrlNetwork({writeToken});
-    }
-
+    // Log object id immediately, in case of error later in script
+    // Don't log hash yet, it will change if --streams was provided (or any other revision to object is needed)
     logger.data("object_id", id);
 
-    if(!status.master_created) {
-      logger.log("Uploading files...");
+    // Close file handles (if any)
+    this.concerns.LocalFile.closeFileHandles(fileHandles);
 
-      const createMasterResponse = await client.CreateProductionMaster({
-        libraryId,
-        type,
-        name: title,
-        fileInfo,
-        encrypt,
-        access,
-        copy,
-        callback: fileConcern.callback,
-        writeToken
-      });
+    logger.errorsAndWarnings(createMasterResponse);
 
-      // Close file handles (if any)
-      this.concerns.LocalFile.closeFileHandles(fileHandles);
+    logger.logList(
+      "",
+      "Production master default variant created:",
+      `  Object ID: ${id}`,
+      `  Write token: ${writeToken}`,
+      ""
+    );
 
-      logger.errorsAndWarnings(createMasterResponse);
-
-      logger.logList(
-        "",
-        "Production master default variant created:",
-        `  Object ID: ${id}`,
-        `  Write token: ${writeToken}`,
-        ""
-      );
-
-      if(!R.isNil(createMasterResponse.errors) && !R.isEmpty(createMasterResponse.errors)) throw Error(`Error(s) encountered while inspecting uploaded files: ${createMasterResponse.errors.join("\n")}`);
-
-      status.master_created = true;
-    } else {
-      this.concerns.LocalFile.closeFileHandles(fileHandles);
-      logger.log(`Skipping upload + master creation (resume), write token ${writeToken}`);
-    }
-
-    SetStatus({state: "master_created", master_created: true});
+    if(!R.isNil(createMasterResponse.errors) && !R.isEmpty(createMasterResponse.errors)) throw Error(`Error(s) encountered while inspecting uploaded files: ${createMasterResponse.errors.join("\n")}`);
 
     // get production master metadata
     const masterMetadata = (await client.ContentObjectMetadata({
@@ -222,35 +103,12 @@ class SampleIngest extends Utility {
     }));
 
     const sources = R.prop("sources", masterMetadata);
-    const variant = R.path(["variants", variantKey], masterMetadata);
-    if(R.isNil(variant)) throw Error(`Variant '${variantKey}' not found in production master metadata`);
+    const variant = R.path(["variants", "default"], masterMetadata);
 
     // add info on source files and variant to data if --json selected
     if(this.args.json) {
       logger.data("media_files", sources);
-      logger.data("variant_key", variantKey);
-      logger.data("variant", variant);
-    }
-
-    // HDR: inject HDR info into first video stream's sources metadata
-    let hdrInfo;
-    if(hdrInfoFile) {
-      hdrInfo = JSON.parse(fs.readFileSync(hdrInfoFile, "utf8"));
-      const fileName = Object.keys(sources)[0];
-      const streams = sources[fileName].streams;
-      const videoStreamIdx = streams.findIndex(s => s.type === "StreamVideo");
-      if(videoStreamIdx < 0) throw Error(`No video stream found in /production_master/sources/${fileName}`);
-
-      streams[videoStreamIdx].hdr = hdrInfo;
-      logger.log(`Adding HDR info to /production_master/sources/${fileName}/streams/${videoStreamIdx}/hdr`);
-
-      await client.ReplaceMetadata({
-        libraryId,
-        objectId: id,
-        writeToken,
-        metadataSubtree: "/production_master/sources",
-        metadata: sources
-      });
+      logger.data("variant_default", variant);
     }
 
     // generate ABR profile
@@ -263,16 +121,6 @@ class SampleIngest extends Utility {
       ABR.ProfileExcludeDRM(genProfileRetVal.result);
     if(!filterProfileRetVal.ok) throw Error(`Error(s) encountered while setting playout formats: ${filterProfileRetVal.errors.join("\n")}`);
 
-    // HDR: add HEVC/10-bit segment specs to the profile
-    if(hdrInfo) {
-      filterProfileRetVal.result.segment_specs = filterProfileRetVal.result.segment_specs || {};
-      filterProfileRetVal.result.segment_specs.video = {
-        ...(filterProfileRetVal.result.segment_specs.video || {}),
-        bit_depth: 10,
-        encoding: "h265"
-      };
-    }
-
     // set up mezzanine offering
     logger.log("Setting up media file conversion...");
     const createMezResponse = await client.CreateABRMezzanine({
@@ -281,8 +129,8 @@ class SampleIngest extends Utility {
       writeToken,
       type,
       masterWriteToken: writeToken,
-      variant: variantKey,
-      offeringKey,
+      variant: "default",
+      offeringKey: "default",
       abrProfile: filterProfileRetVal.result
     });
 
@@ -295,7 +143,7 @@ class SampleIngest extends Utility {
     const startJobsResponse = await client.StartABRMezzanineJobs({
       libraryId,
       objectId: id,
-      offeringKey,
+      offeringKey: "default",
       writeToken
     });
 
@@ -303,25 +151,20 @@ class SampleIngest extends Utility {
     const startJobsErrors = createMezResponse.errors;
     if(!R.isNil(startJobsErrors) && !R.isEmpty(startJobsErrors)) throw Error(`Error(s) encountered while starting file conversion: ${startJobsErrors.join("\n")}`);
 
-    writeToken = startJobsResponse.writeToken || writeToken;
-    nodeUrl = startJobsResponse.nodeUrl || nodeUrl;
-
-    const lroWriteToken = R.path(["lro_draft", "write_token"], startJobsResponse) || writeToken;
-    const lroNode = R.path(["lro_draft", "node"], startJobsResponse) || nodeUrl;
+    const lroWriteToken = R.path(["lro_draft", "write_token"], startJobsResponse);
+    const lroNode = R.path(["lro_draft", "node"], startJobsResponse);
 
     logger.data("library_id", libraryId);
     logger.data("object_id", id);
-    logger.data("offering_key", offeringKey);
+    logger.data("offering_key", "default");
     logger.data("write_token", lroWriteToken);
     logger.data("write_node", lroNode);
-    logger.data("node_endpoint", lroNode);
 
     logger.logList(
       "",
       `Library ID: ${libraryId}`,
       `Object ID: ${id}`,
-      `Variant: ${variantKey}`,
-      `Offering: ${offeringKey}`,
+      "Offering: default",
       `Write Token: ${lroWriteToken}`,
       `Write Node: ${lroNode}`,
       ""
@@ -347,65 +190,27 @@ class SampleIngest extends Utility {
       libraryId,
       objectId: id,
       writeToken,
-      offeringKey
+      offeringKey: "default"
     });
+    const latestHash = finalizeAbrResponse.hash;
 
     logger.errorsAndWarnings(finalizeAbrResponse);
     const finalizeErrors = finalizeAbrResponse.errors;
     if(!R.isNil(finalizeErrors) && !R.isEmpty(finalizeErrors)) throw Error(`Error(s) encountered while finalizing object: ${finalizeErrors.join("\n")}`);
 
-    if(s3Reference) {
-      logger.log("Retaining S3 source file reference.");
-    } else {
-      logger.log("Deleting source files...");
-      await client.DeleteFiles({
-        libraryId,
-        objectId: id,
-        writeToken,
-        filePaths: fileInfo.map(f => f.path)
-      });
-    }
-    nodeUrl = client.WriteTokenNodeUrlLocal({writeToken}) || nodeUrl;
-
-    let latestHash = "not finalized";
-
-    if(noFinalize) {
-      logger.log("Skipping content object finalization (--no-finalize).");
-    } else {
-      logger.log("Finalizing content object...");
-      const sourceFileName = fileInfo[0].path.split("/").pop();
-      const finalizeObjResponse = await client.FinalizeContentObject({
-        libraryId,
-        objectId: id,
-        writeToken,
-        commitMessage: `Ingest ${sourceFileName}${hdrInfoFile ? " [HDR]" : ""}`
-      });
-
-      latestHash = finalizeObjResponse.hash;
-    }
-
-    logger.data("version_hash", latestHash);
-    logger.data("write_token", writeToken);
-    logger.data("write_node", nodeUrl);
-    logger.data("node_endpoint", nodeUrl);
-
     logger.logList(
       "",
-      noFinalize ? "Playable media object draft prepared:" : "Playable media object created:",
+      "Playable media object created:",
       `  Object ID: ${id}`,
       `  Version Hash: ${latestHash}`,
-      `  Write Token: ${writeToken}`,
-      `  Node Endpoint: ${nodeUrl}`,
       ""
     );
-
-    if(!noFinalize) {
-      await this.concerns.Finalize.waitForPublish({
-        latestHash,
-        libraryId,
-        objectId: id
-      });
-    }
+    logger.data("version_hash", latestHash);
+    await this.concerns.Finalize.waitForPublish({
+      latestHash,
+      libraryId,
+      objectId: id
+    });
   }
 
   header() {
